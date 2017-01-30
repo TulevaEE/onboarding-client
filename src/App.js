e7dcef43477dcc7cde25d5a76856c01b1882c06c
Uku Tammet <uku.tammet@transferwise.com>
2017-01-30 14:12:49 +0200

    add basic styleguide

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import React from 'react';
 import logo from './logo.svg';
 
 const App = () => (
-  <div className="hello">
-    I will become the app.
+  <div className="container mt-4">
+    <h1>Main heading</h1>
+    <h2>Secondary heading</h2>
+    <h3>Tertiary heading</h3>
+    <p>I will become the app.</p>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-success">Success</button>
     <img src={logo} alt="Tuleva" />
   </div>
 );
