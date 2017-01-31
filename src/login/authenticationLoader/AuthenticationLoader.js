6f10f0f96dcb487678ac312353cf86cb27b51dfb
Uku Tammet <uku.tammet@transferwise.com>
2017-01-31 10:54:04 +0200

    add cancellation to authentication loader

--- src/login/authenticationLoader/AuthenticationLoader.js
+++ src/login/authenticationLoader/AuthenticationLoader.js
@@ -3,7 +3,7 @@ import { Message } from 'retranslate';
 
 import { Loader } from '../../common';
 
-const AuthenticationLoader = ({ controlCode }) => (
+const AuthenticationLoader = ({ controlCode, onCancel }) => (
   <div className="row mt-4 pt-4 justify-content-center">
     <div className="col-md-8 col-lg-6">
       <div className="card text-center p-4">
@@ -17,18 +17,25 @@ const AuthenticationLoader = ({ controlCode }) => (
             ) : ''
           }
           <Loader />
+          <button className="btn btn-secondary mt-4" onClick={onCancel}>
+            <Message>login.stop</Message>
+          </button>
         </div>
       </div>
     </div>
   </div>
 );
 
+const noop = () => null;
+
 AuthenticationLoader.defaultProps = {
   controlCode: null,
+  onCancel: noop,
 };
 
 AuthenticationLoader.propTypes = {
   controlCode: Types.string,
+  onCancel: Types.func,
 };
 
 export default AuthenticationLoader;
--- src/login/authenticationLoader/AuthenticationLoader.spec.js
+++ src/login/authenticationLoader/AuthenticationLoader.spec.js
@@ -28,4 +28,14 @@ describe('Authenticaion loader', () => {
     expect(component.text()).toContain(controlCode);
     expect(component.contains(<Message>login.control.code</Message>)).toBe(true);
   });
+
+  it('can cancel authentication', () => {
+    const onCancel = jest.fn();
+    component.setProps({ onCancel });
+    const clickButton = () => component.find('button').simulate('click');
+    expect(component.contains(<Message>login.stop</Message>)).toBe(true);
+    expect(onCancel).not.toHaveBeenCalled();
+    clickButton();
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
 });
