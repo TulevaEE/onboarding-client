efbf6c7e63fb29950650c14efb1d24306e9e91ad
Uku Tammet <uku.tammet@transferwise.com>
2017-01-31 16:27:22 +0200

    add requireAuthentication HOC

---
+++ src/requireAuthentication/requireAuthentication.js
@@ -0,0 +1,55 @@
+import React, { Component, PropTypes as Types } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+// higher order component which will redirect to login if tried to go to without auth.
+const requireAuthentication = (WrappedComponent) => {
+  class AuthenticatedComponent extends Component {
+    componentWillMount() {
+      this.checkAuth(this.props.authenticated);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      this.checkAuth(nextProps.authenticated);
+    }
+
+    checkAuth(authenticated) {
+      if (!authenticated) {
+        this.props.redirectToLogin();
+      }
+    }
+
+    render() {
+      return (
+        <div>
+          {this.props.authenticated ? <Component {...this.props} /> : null}
+        </div>
+      );
+    }
+  }
+
+  AuthenticatedComponent.propTypes = {
+    authenticated: Types.bool.isRequired,
+    redirectToLogin: Types.func.isRequired,
+  };
+
+  AuthenticatedComponent.displayName = `requireAuthentication(${getDisplayName(WrappedComponent)})`;
+
+  const mapStateToProps = state => ({
+    authenticated: !!state.login.token,
+  });
+
+  const mapDispatchToProps = dispatch => bindActionCreators({
+    redirectToLogin: () => push('/login'),
+  }, dispatch);
+
+  const connectToRedux = connect(mapStateToProps, mapDispatchToProps);
+  return connectToRedux(AuthenticatedComponent);
+};
+
+export default requireAuthentication;
