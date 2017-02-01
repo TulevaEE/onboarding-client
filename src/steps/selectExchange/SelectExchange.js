23893a4d1ab4f280f84cac6ea71577a5b1fb4ca7
Uku Tammet <uku.tammet@transferwise.com>
2017-02-01 16:07:44 +0200

    add calculation info and step button

--- src/steps/selectExchange/SelectExchange.js
+++ src/steps/selectExchange/SelectExchange.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Message } from 'retranslate';
+import { Link } from 'react-router';
 
 const SelectExchange = () => (
   <div>
-    This will become SelectExchange
+    <div>This will become SelectExchange</div>
+    <Link className="btn btn-primary mt-4 mb-4" to="/steps/select-fund">
+      <Message>steps.next</Message>
+    </Link>
+    <br />
+    <small className="text-muted">
+      <Message>select.exchange.calculation.info</Message>
+    </small>
   </div>
 );
 
