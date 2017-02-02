f1f1c3cd8fe1fc9a3d5ffc138e250bcd29c0c39f
Uku Tammet <uku.tammet@transferwise.com>
2017-02-02 15:22:17 +0200

    add reducer logic for target funds

--- src/exchange/reducer.js
+++ src/exchange/reducer.js
@@ -2,7 +2,14 @@ import {
   GET_SOURCE_FUNDS_START,
   GET_SOURCE_FUNDS_SUCCESS,
   GET_SOURCE_FUNDS_ERROR,
+
   SELECT_EXCHANGE_SOURCES,
+
+  GET_TARGET_FUNDS_START,
+  GET_TARGET_FUNDS_SUCCESS,
+  GET_TARGET_FUNDS_ERROR,
+
+  SELECT_TARGET_FUND,
 } from './constants';
 
 const initialState = {
@@ -10,8 +17,13 @@ const initialState = {
   loadingSourceFunds: false,
   sourceSelection: null,
   sourceSelectionExact: false,
+  targetFunds: null,
+  loadingTargetFunds: false,
+  selectedTargetFund: null,
   error: null,
 };
+
+const DEFAULT_SELECTED_TARGET_FUND_ID = 1;
 
 export default function exchangeReducer(state = initialState, action) {
   switch (action.type) {
@@ -33,6 +45,24 @@ export default function exchangeReducer(state = initialState, action) {
         sourceSelection: action.sourceSelection,
         sourceSelectionExact: !!action.sourceSelectionExact,
       };
+
+    case GET_TARGET_FUNDS_START:
+      return { ...state, loadingTargetFunds: true, error: null };
+    case GET_TARGET_FUNDS_SUCCESS:
+      return {
+        ...state,
+        loadingTargetFunds: false,
+        targetFunds: action.targetFunds,
+        selectedTargetFund: action.targetFunds
+          .reduce((foundFund, current) =>
+            // TODO: find by ISIN, not by id. Do this once we become a fund.
+            foundFund || (current.id === DEFAULT_SELECTED_TARGET_FUND_ID ? current : null), null),
+      };
+    case GET_TARGET_FUNDS_ERROR:
+      return { ...state, loadingTargetFunds: false, error: action.error };
+    case SELECT_TARGET_FUND:
+      return { ...state, selectedTargetFund: action.targetFund };
+
     default:
       return state;
   }
--- src/exchange/reducer.spec.js
+++ src/exchange/reducer.spec.js
@@ -3,7 +3,14 @@ import {
   GET_SOURCE_FUNDS_START,
   GET_SOURCE_FUNDS_SUCCESS,
   GET_SOURCE_FUNDS_ERROR,
+
   SELECT_EXCHANGE_SOURCES,
+
+  GET_TARGET_FUNDS_START,
+  GET_TARGET_FUNDS_SUCCESS,
+  GET_TARGET_FUNDS_ERROR,
+
+  SELECT_TARGET_FUND,
 } from './constants';
 
 describe('Exchange reducer', () => {
@@ -29,12 +36,40 @@ describe('Exchange reducer', () => {
     expect(newState.error).toBe(error);
   });
 
-  it('can select an exchange', () => {
+  it('can select some source funds', () => {
     const sourceSelection = [{ iAmTheSelectedExchange: true }];
     const action = { type: SELECT_EXCHANGE_SOURCES, sourceSelection, sourceSelectionExact: false };
     expect(exchangeReducer(undefined, action).sourceSelection).toEqual(sourceSelection);
     expect(exchangeReducer(undefined, action).sourceSelectionExact).toBe(false);
     action.sourceSelectionExact = true;
     expect(exchangeReducer(undefined, action).sourceSelectionExact).toBe(true);
+  });
+
+  it('starts loading when getting target funds', () => {
+    const action = { type: GET_TARGET_FUNDS_START };
+    expect(exchangeReducer(undefined, action).loadingTargetFunds).toBe(true);
+  });
+
+  it('stops loading, saves and selects target funds when getting them succeeds', () => {
+    const targetFunds = [{ id: 0, hello: true }, { id: 1, iShouldBeSelected: true }];
+    const action = { type: GET_TARGET_FUNDS_SUCCESS, targetFunds };
+    const newState = exchangeReducer({ loadingTargetFunds: true }, action);
+    expect(newState.targetFunds).toEqual(targetFunds);
+    expect(newState.loadingTargetFunds).toBe(false);
+    expect(newState.selectedTargetFund).toEqual(targetFunds[1]);
+  });
+
+  it('stops loading and saves the error when getting target funds fails', () => {
+    const error = new Error('oh no!');
+    const action = { type: GET_TARGET_FUNDS_ERROR, error };
+    const newState = exchangeReducer({ loadingTargetFunds: true }, action);
+    expect(newState.error).toEqual(error);
+    expect(newState.loadingTargetFunds).toBe(false);
+  });
+
+  it('can select a target fund', () => {
+    const targetFund = { thisIsTheTarget: true };
+    const action = { type: SELECT_TARGET_FUND, targetFund };
+    expect(exchangeReducer(undefined, action).selectedTargetFund).toEqual(targetFund);
   });
 });
