4b41744bde0418fbc0f569443fa1f523af667168
Uku Tammet <uku.tammet@transferwise.com>
2017-02-02 17:34:09 +0200

    add Transfer Future Capital page

--- src/steps/transferFutureCapital/TransferFutureCapital.js
+++ src/steps/transferFutureCapital/TransferFutureCapital.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { PropTypes as Types } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Message } from 'retranslate';
+import { Link } from 'react-router';
 
-const TransferFutureCapital = () => (
+import { Radio } from '../../common';
+import { setTransferFutureCapital } from '../../exchange/actions';
+
+export const TransferFutureCapital = ({ transferFutureCapital, onChangeTransferFutureCapital }) => (
   <div>
-    This will become TransferFutureCapital
+    <div className="px-col">
+      <Message>transfer.future.capital.intro</Message>
+    </div>
+    <Radio
+      name="tv-transfer-future-capital"
+      selected={transferFutureCapital}
+      className="mt-4"
+      onSelect={() => onChangeTransferFutureCapital(true)}
+    >
+      <h3><Message>transfer.future.capital.yes</Message></h3>
+    </Radio>
+    <Radio
+      name="tv-transfer-future-capital"
+      selected={!transferFutureCapital}
+      className="mt-2"
+      onSelect={() => onChangeTransferFutureCapital(false)}
+    >
+      <h3><Message>transfer.future.capital.no</Message></h3>
+    </Radio>
+    <div className="px-col mt-4">
+      <Link className="btn btn-primary mr-2" to="/steps/confirm-mandate">
+        <Message>steps.next</Message>
+      </Link>
+      <Link className="btn btn-secondary" to="/steps/select-target-fund">
+        <Message>steps.previous</Message>
+      </Link>
+    </div>
   </div>
 );
 
-export default TransferFutureCapital;
+const noop = () => null;
+
+TransferFutureCapital.defaultProps = {
+  transferFutureCapital: true,
+  onChangeTransferFutureCapital: noop,
+};
+
+TransferFutureCapital.propTypes = {
+  transferFutureCapital: Types.bool,
+  onChangeTransferFutureCapital: Types.func,
+};
+
+const mapStateToProps = state => ({
+  transferFutureCapital: state.exchange.transferFutureCapital,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  onChangeTransferFutureCapital: setTransferFutureCapital,
+}, dispatch);
+
+const connectToRedux = connect(mapStateToProps, mapDispatchToProps);
+
+export default connectToRedux(TransferFutureCapital);
---
+++ src/steps/transferFutureCapital/TransferFutureCapital.spec.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Message } from 'retranslate';
+import { Link } from 'react-router';
+
+import { Radio } from '../../common';
+import { TransferFutureCapital } from './TransferFutureCapital';
+
+describe('Transfer future capital step', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<TransferFutureCapital />);
+  });
+
+  it('shows an intro', () => {
+    expect(component.contains(
+      <Message>
+        transfer.future.capital.intro
+      </Message>,
+    )).toBe(true);
+  });
+
+  it('has buttons to the previous and next pages', () => {
+    expect(component.contains(
+      <Link className="btn btn-primary mr-2" to="/steps/confirm-mandate">
+        <Message>steps.next</Message>
+      </Link>,
+    )).toBe(true);
+    expect(component.contains(
+      <Link className="btn btn-secondary" to="/steps/select-target-fund">
+        <Message>steps.previous</Message>
+      </Link>,
+    )).toBe(true);
+  });
+
+  it('has two options if you want to transfer future capital', () => {
+    expect(component.find(Radio).length).toBe(2);
+    expect(component.find(Radio)
+      .at(0)
+      .childAt(0)
+      .childAt(0)
+      .get(0)).toEqual(<Message>transfer.future.capital.yes</Message>);
+    expect(component.find(Radio)
+      .at(1)
+      .childAt(0)
+      .childAt(0)
+      .get(0)).toEqual(<Message>transfer.future.capital.no</Message>);
+  });
+
+  it('can change if you want to transfer future capital', () => {
+    const radioAtIndexSelected = index => component.find(Radio).at(index).prop('selected');
+    expect(radioAtIndexSelected(0)).toBe(true);
+    expect(radioAtIndexSelected(1)).toBe(false);
+    const selectRadioAtIndex = index => component.find(Radio).at(index).prop('onSelect')();
+    const onChangeTransferFutureCapital = jest.fn();
+    component.setProps({ onChangeTransferFutureCapital });
+
+    expect(onChangeTransferFutureCapital).not.toHaveBeenCalled();
+    selectRadioAtIndex(1);
+    expect(onChangeTransferFutureCapital).toHaveBeenCalledTimes(1);
+    expect(onChangeTransferFutureCapital).toHaveBeenCalledWith(false);
+    onChangeTransferFutureCapital.mockClear();
+
+    component.setProps({ transferFutureCapital: false });
+    expect(radioAtIndexSelected(0)).toBe(false);
+    expect(radioAtIndexSelected(1)).toBe(true);
+
+    expect(onChangeTransferFutureCapital).not.toHaveBeenCalled();
+    selectRadioAtIndex(0);
+    expect(onChangeTransferFutureCapital).toHaveBeenCalledTimes(1);
+    expect(onChangeTransferFutureCapital).toHaveBeenCalledWith(true);
+  });
+});
