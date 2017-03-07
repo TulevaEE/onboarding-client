6fe27df7ea9fbe25e29de6828474cc74f0cca99c
Nikolay Mironenko <nikolay@transferwise.com>
2017-03-07 18:19:39 +0200

    Add terms of use component spec

---
+++ src/termsOfUse/TermsOfUse.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Message } from 'retranslate';
+
+import TermsOfUse from './TermsOfUse';
+
+describe('TermsOfUse', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<TermsOfUse />);
+  });
+
+  it('renders component with terms of use copy', () => {
+    expect(component.contains(<Message>terms.of.use.title</Message>));
+    expect(component.contains(<Message>terms.of.use.subheading</Message>));
+    expect(component.contains(<Message>terms.of.use.list.item.3</Message>));
+  });
+});
