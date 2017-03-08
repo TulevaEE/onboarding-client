bef605065195edd589470bd5d6a9896e7a1f30b3
Uku Tammet <uku.tammet@transferwise.com>
2017-03-08 15:54:55 +0200

    Add skeleton for account page

--- src/account/AccountPage.js
+++ src/account/AccountPage.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Message } from 'retranslate';
 
 export const AccountPage = () => (
-  <div className="row">
-    <div className="col">
-      <h1>I will become the account page.</h1>
+  <div>
+    <div className="row">
+      <div className="col">
+        <h3 className="mt-5"><Message>account.current.balance.title</Message></h3>
+        <p>
+          <Message>account.current.balance.subtitle</Message>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.e-register.ee/"
+          >
+            <Message>account.current.balance.evk</Message>
+          </a>
+        </p>
+
+        PENSION FUND TABLE WILL COME HERE
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h3 className="mt-5"><Message>account.active.fund.title</Message></h3>
+
+        ACTIVE PENSION FUND WILL COME HERE
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h3 className="mt-5"><Message>account.tuleva.balance.title</Message></h3>
+        <p><Message>account.tuleva.balance.subtitle</Message></p>
+        TULEVA BALANCE WILL COME HERE
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h3 className="mt-5"><Message>account.contact.info.title</Message></h3>
+        CONTACT INFO WILL COME HERE
+      </div>
     </div>
   </div>
 );
