310652c0c198a4e6e2f4428a1367e73e02c662ee
Nikolay Mironenko <nikolay@transferwise.com>
2017-03-10 14:33:08 +0200

    Add CalculatorLauncher component test

---
+++ src/steps/selectSources/calculatorLauncher/CalculatorLauncher.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Message } from 'retranslate';
+
+import CalculatorLauncher from './CalculatorLauncher';
+
+describe('Calculator launcher', () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(<CalculatorLauncher />);
+  });
+
+  it('renders main title and button', () => {
+    expect(component.contains(<Message>select.sources.calc.launcher.title</Message>)).toBe(true);
+    expect(component.contains(<Message>select.sources.calc.launcher.button</Message>)).toBe(true);
+  });
+});
