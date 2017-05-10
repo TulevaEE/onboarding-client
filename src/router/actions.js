b088945d34f5285f417df68f0feadeacc88ce2c4
jordan.valdma <jordan.valdma@gmail.com>
2017-05-10 13:14:10 +0300

    routing

--- src/router/actions.js
+++ src/router/actions.js
@@ -7,6 +7,26 @@ function isMember(getState) {
   return false;
 }
 
+function isSelectionComplete(getState) {
+  const userConversion = getState().login.userConversion;
+  if (userConversion.selectionComplete) {
+    return true;
+  }
+  return false;
+}
+
+function isTransfersComplete(getState) {
+  const userConversion = getState().login.userConversion;
+  if (userConversion.transfersComplete) {
+    return true;
+  }
+  return false;
+}
+
+function isFullyConverted(getState) {
+  return isSelectionComplete(getState) && isTransfersComplete(getState);
+}
+
 function isDataLoaded(getState) {
   if (getState().login.user) {
     return true;
@@ -19,7 +39,15 @@ export function selectRouteForState() {
     if (!isDataLoaded(getState)) {
       dispatch(push('/'));
     } else if (isMember(getState)) {
-      dispatch(push('/steps/select-sources'));
+      if (isFullyConverted(getState)) {
+        dispatch(push('/account'));
+      } else if (isSelectionComplete(getState)) {
+        dispatch(push('/steps/select-sources'));
+      } else if (isTransfersComplete(getState)) {
+        dispatch(push('/steps/transfer-future-capital'));
+      } else {
+        dispatch(push('/steps/select-sources'));
+      }
     } else {
       dispatch(push('/steps/new-user'));
     }
--- src/router/actions.spec.js
+++ src/router/actions.spec.js
@@ -25,14 +25,46 @@ describe('Routing actions', () => {
     mockDispatch();
   });
 
-  it('can perform a member routing', () => {
+  it('can perform routing for members who have not completed transfers', () => {
     state.login.user.memberNumber = 123;
+    state.login.userConversion = {
+      transfersComplete: false,
+      selectionComplete: true,
+    };
 
     const action = createBoundAction(actions.selectRouteForState);
     action();
 
     expect(dispatch).toHaveBeenCalledTimes(1);
     expect(dispatch).toHaveBeenCalledWith(push('/steps/select-sources'));
+  });
+
+  it('can perform routing for members who have completed transfers but not completed selection', () => {
+    state.login.user.memberNumber = 123;
+    state.login.userConversion = {
+      transfersComplete: true,
+      selectionComplete: false,
+    };
+
+    const action = createBoundAction(actions.selectRouteForState);
+    action();
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith(push('/steps/transfer-future-capital'));
+  });
+
+  it('can perform routing for fully converted members', () => {
+    state.login.user.memberNumber = 123;
+    state.login.userConversion = {
+      transfersComplete: true,
+      selectionComplete: true,
+    };
+
+    const action = createBoundAction(actions.selectRouteForState);
+    action();
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith(push('/account'));
   });
 
   it('can perform non member routing', () => {
