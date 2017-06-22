b47651ad2347d92ff781dbbcd4d8fe2c86dc8514
jordan.valdma <jordan.valdma@gmail.com>
2017-06-22 08:59:25 +0300

    Fixes shop id

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ export default function initializeConfiguration() {
     config.set({
       mixpanelKey: 'be355b17352ad6b4660ae595cd65ce61',
       applicationUrl: 'http://localhost:3000',
-      newUserPaymentRedirectBaseUrl: 'https://payment-test.maksekeskus.ee/pay/1/link.html',
+      newUserPaymentRedirectBaseUrl: 'https://payment-test.maksekeskus.ee/pay/1/link.html?shopId=322a5e5e-37ee-45b1-8961-ebd00e84e209',
     }, { freeze: false });
   }
 
@@ -13,7 +13,7 @@ export default function initializeConfiguration() {
     config.set({
       mixpanelKey: 'ff49493de3c6ed27e198e6b15063b60f',
       applicationUrl: 'https://pension.tuleva.ee',
-      newUserPaymentRedirectBaseUrl: 'https://payment.maksekeskus.ee/pay/1/link.html',
+      newUserPaymentRedirectBaseUrl: 'https://payment.maksekeskus.ee/pay/1/link.html?shopId=8c1a6c0a-2467-4166-ab47-006ddbf38b06',
     }, { freeze: false });
   }
 }
--- src/newUserFlow/payment/Payment.spec.js
+++ src/newUserFlow/payment/Payment.spec.js
@@ -22,7 +22,7 @@ describe('Payment', () => {
     component.setProps({ userId: 'test123' });
 
     expect(component.contains(<a
-      href={ newUserPaymentRedirectBaseUrl + "?shopId=8c1a6c0a-2467-4166-ab47-006ddbf38b06&amount=100&reference=test123&return_url=https://onboarding-service.tuleva.ee/notifications/payments&return_method=POST&cancel_url=https://pension.tuleva.ee/steps/payment&cancel_method=GET&notification_url=https://onboarding-service.tuleva.ee/notifications/payments&notification_method=POST"}
+      href={ newUserPaymentRedirectBaseUrl + "&amount=100&reference=test123&return_url=https://onboarding-service.tuleva.ee/notifications/payments&return_method=POST&cancel_url=https://pension.tuleva.ee/steps/payment&cancel_method=GET&notification_url=https://onboarding-service.tuleva.ee/notifications/payments&notification_method=POST"}
       className="btn btn-primary"
     ><Message>new.user.flow.payment.bank.links</Message></a>)).toBe(true);
   });
