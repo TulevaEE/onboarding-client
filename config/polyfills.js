7bcc2e0f6d6c74a0068ae9eee48c95857b1d9a08
Erko Risthein <erko@risthein.ee>
2017-07-26 10:51:33 +0300

    Added array.prototype.find polyfill

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,13 +1,4 @@
-var Raven = require('raven-js');
-
-if (process.env.NODE_ENV === 'production') {
-  Raven
-    .config('https://cfcb0c4bb8cb4264942f80ca1eb78c49@sentry.io/146907', {
-      release: process.env.HEROKU_SLUG_COMMIT,
-      environment: process.env.NODE_ENV,
-    })
-    .install();
-}
+require('array.prototype.find').shim();
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
@@ -29,3 +20,14 @@ require('whatwg-fetch');
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
+
+var Raven = require('raven-js');
+
+if (process.env.NODE_ENV === 'production') {
+    Raven
+        .config('https://cfcb0c4bb8cb4264942f80ca1eb78c49@sentry.io/146907', {
+            release: process.env.HEROKU_SLUG_COMMIT,
+            environment: process.env.NODE_ENV,
+        })
+        .install();
+}
