f90ca28bb4548b874fad17dcb29479add95170b8
Jordan Valdma <jordan.valdma@gmail.com>
2017-09-03 18:14:50 +0300

    Updates acme challenge

--- scripts/start.js
+++ scripts/start.js
@@ -20,8 +20,8 @@ app.use(forceHttps);
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use('/api', proxy('https://onboarding-service.tuleva.ee'));
 
-app.get('/.well-known/acme-challenge/A-yh1OpXH71BPS6LZ6MKlIL4S7mujc76Wy8LyUszokU', (request, response) =>
-  response.send('A-yh1OpXH71BPS6LZ6MKlIL4S7mujc76Wy8LyUszokU.EMEBBxvSam3n_ien1J0z4dXeTuc2JuR3HqfAP6teLjE'));
+app.get('/.well-known/acme-challenge/2naNo4FcSHjguZOAi4aISIQkV8OxHyz3kqr8Zf4Smlg', (request, response) =>
+  response.send('2naNo4FcSHjguZOAi4aISIQkV8OxHyz3kqr8Zf4Smlg.EMEBBxvSam3n_ien1J0z4dXeTuc2JuR3HqfAP6teLjE'));
 
 app.get('*', (request, response) =>
   response.sendFile(path.join(__dirname, '..', 'build', 'index.html')));
