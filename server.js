65daee5c410464731d278ecfe3cf2c09b6ec1ace
Erko Risthein <erko@risthein.ee>
2018-12-03 13:09:29 +0200

    Remove forceHttps temporarily

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function forceHttps(request, response, next) {
 }
 
 app.use(compression());
-app.use(forceHttps);
+// app.use(forceHttps);
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api', proxy('https://onboarding-service.tuleva.ee'));
 
