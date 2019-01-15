fac60b8e0750c0328ac9069dc62aa3da18d5b541
Erko Risthein <erko@risthein.ee>
2019-01-15 10:35:28 +0200

    Hotfix: Remove this year option from comparison calculator

--- src/entries/components/returnComparison/options.js
+++ src/entries/components/returnComparison/options.js
@@ -8,13 +8,13 @@ export default function getOptions() {
   const fiveYearsAgo = format(moment().subtract(5, 'years'));
   const threeYearsAgo = format(moment().subtract(3, 'years'));
   const oneYearAgo = format(moment().subtract(1, 'year'));
-  const thisYear = format(moment().startOf('year'));
+  // const thisYear = format(moment().startOf('year'));
 
   return [
     { value: startDate, label: 'returnComparison.period.all' },
     { value: fiveYearsAgo, label: 'returnComparison.period.fiveYears' },
     { value: threeYearsAgo, label: 'returnComparison.period.threeYears' },
     { value: oneYearAgo, label: 'returnComparison.period.oneYear' },
-    { value: thisYear, label: 'returnComparison.period.thisYear' },
+    // { value: thisYear, label: 'returnComparison.period.thisYear' },
   ];
 }
