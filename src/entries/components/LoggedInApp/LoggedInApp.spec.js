7d7e735d83c8a53e054e13ebb3a31c19c2ffa777
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2019-05-15 12:00:41 +0300

    Rename App to LoggedInApp

--- src/entries/components/LoggedInApp/LoggedInApp.spec.js
+++ src/entries/components/LoggedInApp/LoggedInApp.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { App } from './App';
+import { App } from './LoggedInApp';
 import Header from './header';
 import Footer from './footer';
 
