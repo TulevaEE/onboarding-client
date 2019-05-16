da518545fa1c6225853c3b2a09f7ae40c2d2f592
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2019-05-16 17:37:04 +0300

    Add LHV Index Plus fund to third pillar target funds

--- src/entries/components/thirdPillar/reducer.js
+++ src/entries/components/thirdPillar/reducer.js
@@ -4,7 +4,7 @@ import {
   CHANGE_EXCHANGE_EXISTING_UNITS,
 } from './constants';
 import initialState from './initialState';
-import { GET_SOURCE_FUNDS_SUCCESS } from '../exchange/constants';
+import { GET_SOURCE_FUNDS_SUCCESS, GET_TARGET_FUNDS_SUCCESS } from '../exchange/constants';
 
 export default function thirdPillarReducer(state = initialState, action) {
   const { type, query, monthlyContribution } = action;
@@ -28,12 +28,17 @@ export default function thirdPillarReducer(state = initialState, action) {
       // eslint-disable-next-line no-case-declarations
       const exchangeExistingUnits = sourceFunds.length === 0 ? false : state.exchangeExistingUnits;
 
+      return { ...state, sourceFunds, exchangeExistingUnits };
+    case GET_TARGET_FUNDS_SUCCESS:
       return {
         ...state,
-        sourceFunds,
-        exchangeExistingUnits,
+        targetFunds: action.targetFunds.filter(isLhvIndexPlusFund),
       };
     default:
       return state;
   }
+}
+
+function isLhvIndexPlusFund(fund) {
+  return fund.isin === 'EE3600109419';
 }
--- src/entries/components/thirdPillar/reducer.spec.js
+++ src/entries/components/thirdPillar/reducer.spec.js
@@ -5,7 +5,7 @@ import {
 } from './constants';
 import initialState from './initialState';
 import reducer from './reducer';
-import { GET_SOURCE_FUNDS_SUCCESS } from '../exchange/constants';
+import { GET_SOURCE_FUNDS_SUCCESS, GET_TARGET_FUNDS_SUCCESS } from '../exchange/constants';
 
 describe('Third pillar reducer', () => {
   it('saves monthly contribution as a number when monthlyContribution is in query params and parsable as a number', () => {
@@ -150,6 +150,18 @@ describe('Third pillar reducer', () => {
       ...initialState,
       sourceFunds: [],
       exchangeExistingUnits: false,
+    });
+  });
+
+  it('updates target funds to include only lhv index plus fund on success', () => {
+    const state = reducer(undefined, {
+      type: GET_TARGET_FUNDS_SUCCESS,
+      targetFunds: [{ isin: 'EE123', pillar: 3 }, { isin: 'EE3600109419', pillar: 3 }],
+    });
+
+    expect(state).toEqual({
+      ...initialState,
+      targetFunds: [{ isin: 'EE3600109419', pillar: 3 }],
     });
   });
 });
