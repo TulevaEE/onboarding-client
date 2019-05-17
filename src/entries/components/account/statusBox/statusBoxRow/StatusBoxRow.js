7babdbdfb6f4b1cfec866f6bb4d40645fb2e1bf8
Mati Vait <mativait@gmail.com>
2019-05-17 16:46:15 +0300

    design fix

--- src/entries/components/account/statusBox/statusBoxRow/StatusBoxRow.js
+++ src/entries/components/account/statusBox/statusBoxRow/StatusBoxRow.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { PropTypes as Types } from 'prop-types';
 import { Message } from 'retranslate';
+import './StatusBoxRow.scss';
+
+const CheckMark = ({ checked }) => (
+  <div className="status-box-row pt-1">{checked ? '✔' : '🗙'} </div>
+);
+CheckMark.defaultProps = {
+  checked: false,
+};
+
+CheckMark.propTypes = {
+  checked: Types.bool,
+};
 
 const StatusBoxRow = ({ name, lines, showAction, ok, children, last }) => {
   const displayName = <Message>{name}</Message>;
   const formattedLines = (
-    <ul>
-      {lines.map(line => {
-        return line;
-      })}
+    <ul className="status-box-row">
+      {lines &&
+        lines.length > 0 &&
+        lines.map(line => {
+          return (
+            <li className="status-box-row pl-2" key={line}>
+              {line}
+            </li>
+          );
+        })}
     </ul>
   );
   return (
     <div className={`row  py-2 ${!last ? 'tv-table__row' : ''}`}>
-      <div className="col-12 col-sm">
-        {ok ? '✔' : '🗙'} <b>{displayName}</b>
-        {formattedLines}
+      <div className="media">
+        <CheckMark checked={ok} />
+        <div className="media-body">
+          <div className="mt-0 pt-1 pl-2">
+            <b>{displayName}</b>
+          </div>
+          {formattedLines}
+        </div>
       </div>
       <div className="col-12 col-sm text-sm-right">{showAction && children ? children : ''}</div>
     </div>
