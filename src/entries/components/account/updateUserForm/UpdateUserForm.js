89c03ccbe2aee55c4d38df5f28dae035a949373f
Erko Risthein <erko@risthein.ee>
2019-06-04 14:10:34 +0300

    Conditionally render the district field

--- src/entries/components/account/updateUserForm/UpdateUserForm.js
+++ src/entries/components/account/updateUserForm/UpdateUserForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PropTypes as Types } from 'prop-types';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, formValueSelector, reduxForm } from 'redux-form';
 import { Message, withTranslations } from 'retranslate';
 
 import { requiredField, emailValidator, renderField } from '../../common/form';
@@ -12,6 +12,7 @@ export const UpdateUserForm = ({
   invalid,
   submitting,
   error,
+  isCountryEstonia,
   translations: { translate },
 }) => (
   <div>
@@ -64,82 +65,6 @@ export const UpdateUserForm = ({
               placeholder={translate('new.user.flow.signup.address')}
               validate={[requiredField]}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="update-user-form-district">
-              <Message>new.user.flow.signup.district</Message>
-            </label>
-            <Field
-              component={renderField}
-              type="select"
-              name="address.districtCode"
-              id="update-user-form-district"
-              validate={[requiredField]}
-            >
-              <option />
-              <option value="1060">Abja-Paluoja linn</option>
-              <option value="1301">Antsla linn</option>
-              <option value="0170">Elva linn</option>
-              <option value="0183">Haapsalu linn</option>
-              <option value="0037">Harju maakond</option>
-              <option value="0039">Hiiu maakond</option>
-              <option value="0044">Ida-Viru maakond</option>
-              <option value="0051">Järva maakond</option>
-              <option value="0249">Jõgeva linn</option>
-              <option value="0049">Jõgeva maakond</option>
-              <option value="2270">Jõhvi linn</option>
-              <option value="0279">Kallaste linn</option>
-              <option value="3895">Kärdla linn</option>
-              <option value="2761">Karksi-Nuia linn</option>
-              <option value="2928">Kehra linn</option>
-              <option value="0296">Keila linn</option>
-              <option value="3083">Kilingi-Nõmme linn</option>
-              <option value="0309">Kiviõli linn</option>
-              <option value="0322">Kohtla-Järve linn</option>
-              <option value="0345">Kunda linn</option>
-              <option value="0349">Kuressaare linn</option>
-              <option value="0057">Lääne maakond</option>
-              <option value="0059">Lääne-Viru maakond</option>
-              <option value="4330">Lihula linn</option>
-              <option value="0424">Loksa linn</option>
-              <option value="0446">Maardu linn</option>
-              <option value="0490">Mõisaküla linn</option>
-              <option value="0485">Mustvee linn</option>
-              <option value="0513">Narva-Jõesuu linn</option>
-              <option value="0511">Narva linn</option>
-              <option value="5754">Otepää linn</option>
-              <option value="0566">Paide linn</option>
-              <option value="0580">Paldiski linn</option>
-              <option value="0625">Pärnu linn</option>
-              <option value="0067">Pärnu maakond</option>
-              <option value="0617">Põltsamaa linn</option>
-              <option value="6536">Põlva linn</option>
-              <option value="0065">Põlva maakond</option>
-              <option value="6671">Püssi linn</option>
-              <option value="0663">Rakvere linn</option>
-              <option value="7216">Räpina linn</option>
-              <option value="6826">Rapla linn</option>
-              <option value="0070">Rapla maakond</option>
-              <option value="0074">Saare maakond</option>
-              <option value="0728">Saue linn</option>
-              <option value="0735">Sillamäe linn</option>
-              <option value="0741">Sindi linn</option>
-              <option value="7836">Suure-Jaani linn</option>
-              <option value="0784">Tallinn</option>
-              <option value="8130">Tamsalu linn</option>
-              <option value="8140">Tapa linn</option>
-              <option value="0795">Tartu linn</option>
-              <option value="0078">Tartu maakond</option>
-              <option value="0823">Tõrva linn</option>
-              <option value="8595">Türi linn</option>
-              <option value="0854">Valga linn</option>
-              <option value="0082">Valga maakond</option>
-              <option value="0897">Viljandi linn</option>
-              <option value="0084">Viljandi maakond</option>
-              <option value="0912">Võhma linn</option>
-              <option value="0919">Võru linn</option>
-              <option value="0086">Võru maakond</option>
-            </Field>
           </div>
           <div className="form-group">
             <label htmlFor="update-user-form-postalCode">
@@ -417,6 +342,84 @@ export const UpdateUserForm = ({
               <option value="ZW">Zimbabwe</option>
             </Field>
           </div>
+          {isCountryEstonia && (
+            <div className="form-group">
+              <label htmlFor="update-user-form-district">
+                <Message>new.user.flow.signup.district</Message>
+              </label>
+              <Field
+                component={renderField}
+                type="select"
+                name="address.districtCode"
+                id="update-user-form-district"
+                validate={[requiredField]}
+              >
+                <option />
+                <option value="1060">Abja-Paluoja linn</option>
+                <option value="1301">Antsla linn</option>
+                <option value="0170">Elva linn</option>
+                <option value="0183">Haapsalu linn</option>
+                <option value="0037">Harju maakond</option>
+                <option value="0039">Hiiu maakond</option>
+                <option value="0044">Ida-Viru maakond</option>
+                <option value="0051">Järva maakond</option>
+                <option value="0249">Jõgeva linn</option>
+                <option value="0049">Jõgeva maakond</option>
+                <option value="2270">Jõhvi linn</option>
+                <option value="0279">Kallaste linn</option>
+                <option value="3895">Kärdla linn</option>
+                <option value="2761">Karksi-Nuia linn</option>
+                <option value="2928">Kehra linn</option>
+                <option value="0296">Keila linn</option>
+                <option value="3083">Kilingi-Nõmme linn</option>
+                <option value="0309">Kiviõli linn</option>
+                <option value="0322">Kohtla-Järve linn</option>
+                <option value="0345">Kunda linn</option>
+                <option value="0349">Kuressaare linn</option>
+                <option value="0057">Lääne maakond</option>
+                <option value="0059">Lääne-Viru maakond</option>
+                <option value="4330">Lihula linn</option>
+                <option value="0424">Loksa linn</option>
+                <option value="0446">Maardu linn</option>
+                <option value="0490">Mõisaküla linn</option>
+                <option value="0485">Mustvee linn</option>
+                <option value="0513">Narva-Jõesuu linn</option>
+                <option value="0511">Narva linn</option>
+                <option value="5754">Otepää linn</option>
+                <option value="0566">Paide linn</option>
+                <option value="0580">Paldiski linn</option>
+                <option value="0625">Pärnu linn</option>
+                <option value="0067">Pärnu maakond</option>
+                <option value="0617">Põltsamaa linn</option>
+                <option value="6536">Põlva linn</option>
+                <option value="0065">Põlva maakond</option>
+                <option value="6671">Püssi linn</option>
+                <option value="0663">Rakvere linn</option>
+                <option value="7216">Räpina linn</option>
+                <option value="6826">Rapla linn</option>
+                <option value="0070">Rapla maakond</option>
+                <option value="0074">Saare maakond</option>
+                <option value="0728">Saue linn</option>
+                <option value="0735">Sillamäe linn</option>
+                <option value="0741">Sindi linn</option>
+                <option value="7836">Suure-Jaani linn</option>
+                <option value="0784">Tallinn</option>
+                <option value="8130">Tamsalu linn</option>
+                <option value="8140">Tapa linn</option>
+                <option value="0795">Tartu linn</option>
+                <option value="0078">Tartu maakond</option>
+                <option value="0823">Tõrva linn</option>
+                <option value="8595">Türi linn</option>
+                <option value="0854">Valga linn</option>
+                <option value="0082">Valga maakond</option>
+                <option value="0897">Viljandi linn</option>
+                <option value="0084">Viljandi maakond</option>
+                <option value="0912">Võhma linn</option>
+                <option value="0919">Võru linn</option>
+                <option value="0086">Võru maakond</option>
+              </Field>
+            </div>
+          )}
           <div className={`form-group ${error ? 'has-danger' : ''}`}>
             {error && <div className="form-control-feedback mb-3">{error}</div>}
             <button
@@ -441,6 +444,7 @@ UpdateUserForm.defaultProps = {
   submitting: false,
   error: '',
   updateUserSuccess: false,
+  isCountryEstonia: true,
 };
 
 UpdateUserForm.propTypes = {
@@ -450,14 +454,17 @@ UpdateUserForm.propTypes = {
   error: Types.string,
   translations: Types.shape({ translate: Types.func.isRequired }).isRequired,
   updateUserSuccess: Types.bool,
+  isCountryEstonia: Types.bool,
 };
 
 const reduxSignUpForm = reduxForm({ form: 'updateUser' })(UpdateUserForm);
 const translatedForm = withTranslations(reduxSignUpForm);
+const selector = formValueSelector('updateUser');
 
 const mapStateToProps = state => ({
   initialValues: state.login.user ? { ...state.login.user } : null,
   updateUserSuccess: state.account.updateUserSuccess,
+  isCountryEstonia: selector(state, 'address.countryCode') === 'EE',
 });
 
 const connectToRedux = connect(
