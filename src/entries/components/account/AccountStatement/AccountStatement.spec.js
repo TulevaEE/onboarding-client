1f46292380eb6ce081ebb303b94dda95a6384a81
Oliver Viljamaa <oliverviljamaa@users.noreply.github.com>
2019-08-27 17:12:18 +0300

    Add contributions and profit columns to account statements (#247)

--- src/entries/components/account/AccountStatement/AccountStatement.spec.js
+++ src/entries/components/account/AccountStatement/AccountStatement.spec.js
@@ -12,9 +12,9 @@ describe('Account statement', () => {
     component = shallow(
       <AccountStatement
         funds={[
-          { isin: 'A1', name: 'A', price: 100 },
-          { isin: 'B2', name: 'B', price: 10, activeFund: true },
-          { isin: 'C3', name: 'C', price: 1 },
+          { isin: 'A1', name: 'A', contributionSum: 0, profit: 0, price: 100 },
+          { isin: 'B2', name: 'B', contributionSum: 0, profit: 0, price: 10, activeFund: true },
+          { isin: 'C3', name: 'C', contributionSum: 0, profit: 0, price: 1 },
         ]}
       />,
     );
@@ -24,13 +24,13 @@ describe('Account statement', () => {
     expect(funds).toEqual(['A', 'B*', 'C']);
   });
 
-  it('passes total as value column footer', () => {
+  it('passes total contributions as contribution column footer', () => {
     component = shallow(
       <AccountStatement
         funds={[
-          { isin: 'A1', name: 'A', price: 100 },
-          { isin: 'B2', name: 'B', price: 10 },
-          { isin: 'C3', name: 'C', price: 1 },
+          { isin: 'A1', name: 'A', contributionSum: 100, profit: 0, price: 0 },
+          { isin: 'B2', name: 'B', contributionSum: 10, profit: 0, price: 0 },
+          { isin: 'C3', name: 'C', contributionSum: 1, profit: 0, price: 0 },
         ]}
       />,
     );
@@ -38,6 +38,38 @@ describe('Account statement', () => {
     const { footer } = tableProp('columns')[1];
 
     expect(footer).toEqual(<Euro amount={111} />);
+  });
+
+  it('passes total profit as profit column footer', () => {
+    component = shallow(
+      <AccountStatement
+        funds={[
+          { isin: 'A1', name: 'A', contributionSum: 0, profit: 200, price: 0 },
+          { isin: 'B2', name: 'B', contributionSum: 0, profit: 20, price: 0 },
+          { isin: 'C3', name: 'C', contributionSum: 0, profit: 2, price: 0 },
+        ]}
+      />,
+    );
+
+    const { footer } = tableProp('columns')[2];
+
+    expect(footer).toEqual(<Euro amount={222} />);
+  });
+
+  it('passes total value as value column footer', () => {
+    component = shallow(
+      <AccountStatement
+        funds={[
+          { isin: 'A1', name: 'A', contributionSum: 0, profit: 0, price: 300 },
+          { isin: 'B2', name: 'B', contributionSum: 0, profit: 0, price: 30 },
+          { isin: 'C3', name: 'C', contributionSum: 0, profit: 0, price: 3 },
+        ]}
+      />,
+    );
+
+    const { footer } = tableProp('columns')[3];
+
+    expect(footer).toEqual(<Euro amount={333} />);
   });
 
   const tableProp = name => component.find(Table).prop(name);
