4573cde9e071b4f88b71010f40fc8167abb0f8e2
Erko Risthein <erko@risthein.ee>
2019-10-03 13:47:36 +0200

    New 3rd pillar setup page with the correct redirect logic

--- src/entries/components/flows/thirdPillar/ThirdPillarSetup/ThirdPillarSetup.js
+++ src/entries/components/flows/thirdPillar/ThirdPillarSetup/ThirdPillarSetup.js
@@ -1,57 +1,21 @@
 import React from 'react';
 import Types from 'prop-types';
-import { bindActionCreators } from 'redux';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Message } from 'retranslate';
 
-import { actions as thirdPillarActions } from '../../../thirdPillar';
-
-export const ThirdPillarSetup = ({
-  monthlyContribution,
-  onMonthlyContributionChange,
-  exchangeableSourceFunds,
-  exchangeExistingUnits,
-  onExchangeExistingUnitsChange,
-  nextPath,
-  hasPensionAccount,
-}) => (
+export const ThirdPillarSetup = ({ nextPath, isThirdPillarActive }) => (
   <div>
-    {hasPensionAccount && <Redirect to={nextPath} />}
-    <div className="form-group">
-      <label htmlFor="monthly-contribution">
-        <Message>thirdPillarFlowSetup.monthlyContributionLabel</Message>
-      </label>
-      <input
-        id="monthly-contribution"
-        type="number"
-        value={monthlyContribution || ''}
-        onChange={event => {
-          onMonthlyContributionChange(parseInt(event.target.value, 10));
-        }}
-        className="form-control form-control-lg"
-      />
-    </div>
-
-    {exchangeableSourceFunds && exchangeableSourceFunds.length > 0 && (
-      <div className="custom-control custom-checkbox mt-3">
-        <input
-          checked={exchangeExistingUnits}
-          onChange={() => onExchangeExistingUnitsChange(!exchangeExistingUnits)}
-          type="checkbox"
-          className="custom-control-input"
-          id="exchange-existing-units-checkbox"
-        />
-
-        <label className="custom-control-label" htmlFor="exchange-existing-units-checkbox">
-          <Message>thirdPillarFlowSetup.exchangeExistingUnitsLabel</Message>
-        </label>
-      </div>
-    )}
-
+    {isThirdPillarActive && <Redirect to={nextPath} />}
+    <p>
+      <Message>thirdPillarFlowSetup.text</Message>
+    </p>
+    <p>
+      <Message>thirdPillarFlowSetup.subtext</Message>
+    </p>
     <div>
       <Link to={nextPath}>
-        <button type="button" className="btn btn-primary mt-2" disabled={!monthlyContribution}>
+        <button type="button" className="btn btn-primary mt-2">
           <Message>thirdPillarFlowSetup.buttonText</Message>
         </button>
       </Link>
@@ -60,48 +24,21 @@ export const ThirdPillarSetup = ({
 );
 
 ThirdPillarSetup.propTypes = {
-  monthlyContribution: Types.number,
-  onMonthlyContributionChange: Types.func,
-  exchangeableSourceFunds: Types.arrayOf(Types.shape()),
-  exchangeExistingUnits: Types.bool,
-  onExchangeExistingUnitsChange: Types.func,
   nextPath: Types.string,
-  hasPensionAccount: Types.bool,
+  isThirdPillarActive: Types.bool,
 };
 
-const noop = () => {};
-
 ThirdPillarSetup.defaultProps = {
-  monthlyContribution: null,
-  onMonthlyContributionChange: noop,
-  exchangeableSourceFunds: null,
-  exchangeExistingUnits: false,
-  onExchangeExistingUnitsChange: noop,
   nextPath: '',
-  hasPensionAccount: false,
+  isThirdPillarActive: false,
 };
 
 const mapStateToProps = state => ({
-  monthlyContribution: state.thirdPillar.monthlyContribution,
-  exchangeExistingUnits: state.thirdPillar.exchangeExistingUnits,
-  exchangeableSourceFunds: state.thirdPillar.exchangeableSourceFunds,
-  hasPensionAccount: !!(
+  isThirdPillarActive: !!(
     state.login.token &&
     state.login.user &&
-    state.login.user.pensionAccountNumber
+    state.login.user.thirdPillarActive
   ),
 });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      onMonthlyContributionChange: thirdPillarActions.changeMonthlyContribution,
-      onExchangeExistingUnitsChange: thirdPillarActions.changeExchangeExistingUnits,
-    },
-    dispatch,
-  );
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ThirdPillarSetup);
+export default connect(mapStateToProps)(ThirdPillarSetup);
--- src/entries/components/flows/thirdPillar/ThirdPillarSetup/ThirdPillarSetup.spec.js
+++ src/entries/components/flows/thirdPillar/ThirdPillarSetup/ThirdPillarSetup.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import { ThirdPillarSetup } from './ThirdPillarSetup';
@@ -10,68 +10,18 @@ describe('ThirdPillarSetup', () => {
     component = shallow(<ThirdPillarSetup />);
   });
 
-  it('has monthly contribution', () => {
-    const value = () => contributionInput().prop('value');
+  it('redirects to next path when the 3rd pillar is active', () => {
+    component.setProps({ nextPath: '/next-path' });
+    const redirects = () => component.contains(<Redirect to="/next-path" />);
 
-    expect(value()).toBe('');
-    component.setProps({ monthlyContribution: 1 });
-    expect(value()).toBe(1);
-  });
-
-  it('calls change handler on monthly contribution change', () => {
-    const onMonthlyContributionChange = jest.fn();
-    component.setProps({ onMonthlyContributionChange });
-
-    expect(onMonthlyContributionChange).not.toBeCalled();
-    contributionInput().simulate('change', { target: { value: '1' } });
-    expect(onMonthlyContributionChange).toBeCalledWith(1);
-  });
-
-  it('has exchange existing units checkbox when more than one exchangeable source fund', () => {
-    const hasCheckbox = () => exchangeExistingUnitsCheckbox().exists();
-
-    expect(hasCheckbox()).toBe(false);
-    component.setProps({ exchangeableSourceFunds: [{ isin: 'EE123' }] });
-    expect(hasCheckbox()).toBe(true);
-  });
-
-  it('sets exchange existing units checkbox value based on passed prop', () => {
-    component.setProps({ exchangeableSourceFunds: [{ isin: 'EE123' }] });
-
-    const checked = () => exchangeExistingUnitsCheckbox().prop('checked');
-
-    expect(checked()).toBe(false);
-    component.setProps({ exchangeExistingUnits: true });
-    expect(checked()).toBe(true);
-  });
-
-  it('calls change handler on exchange existing units change', () => {
-    component.setProps({ exchangeableSourceFunds: [{ isin: 'EE123' }] });
-
-    const onExchangeExistingUnitsChange = jest.fn();
-    component.setProps({ onExchangeExistingUnitsChange });
-
-    expect(onExchangeExistingUnitsChange).not.toBeCalled();
-    exchangeExistingUnitsCheckbox().simulate('change');
-    expect(onExchangeExistingUnitsChange).toBeCalledWith(true);
-    component.setProps({ exchangeExistingUnits: true });
-    exchangeExistingUnitsCheckbox().simulate('change');
-    expect(onExchangeExistingUnitsChange).toBeCalledWith(false);
-  });
-
-  it('disables button when no monthly contribution', () => {
-    const buttonIsDisabled = () => component.find('button').prop('disabled');
-
-    expect(buttonIsDisabled()).toBe(true);
-    component.setProps({ monthlyContribution: 1 });
-    expect(buttonIsDisabled()).toBe(false);
+    component.setProps({ isThirdPillarActive: false });
+    expect(redirects()).toBe(false);
+    component.setProps({ isThirdPillarActive: true });
+    expect(redirects()).toBe(true);
   });
 
   it('redirects to next path on button click', () => {
     component.setProps({ nextPath: '/a-path' });
     expect(component.find(Link).prop('to')).toBe('/a-path');
   });
-
-  const contributionInput = () => component.find('#monthly-contribution');
-  const exchangeExistingUnitsCheckbox = () => component.find('#exchange-existing-units-checkbox');
 });
