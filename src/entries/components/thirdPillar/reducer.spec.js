81c23d1ba0e96b205e90c72e1be45470cbe6810d
Erko Risthein <erko@risthein.ee>
2019-10-15 12:40:32 +0300

    Revert "Filter out zero balance 3rd pillar funds from exchangeable source funds"
    This reverts commit 49df114f

--- src/entries/components/thirdPillar/reducer.spec.js
+++ src/entries/components/thirdPillar/reducer.spec.js
@@ -126,20 +126,19 @@ describe('Third pillar reducer', () => {
       selectedFutureContributionsFundIsin: 'EE789',
     };
 
-    const thirdPillarFund = { isin: 'EE123', pillar: 3, value: 100 };
-    const secondPillarFund = { isin: 'EE456', pillar: 2, value: 100 };
-    const anotherThirdPillarFund = { isin: 'EE789', pillar: 3, value: 100 };
-    const zeroThirdPillarFund = { isin: 'EE789', pillar: 3, value: 0 };
+    const thirdPillarFund = { isin: 'EE123', pillar: 3 };
+    const secondPillarFund = { isin: 'EE456', pillar: 2 };
+    const anotherThirdPillarFund = { isin: 'EE789', pillar: 3 };
     const state = reducer(oldState, {
       type: GET_SOURCE_FUNDS_SUCCESS,
-      sourceFunds: [thirdPillarFund, secondPillarFund, anotherThirdPillarFund, zeroThirdPillarFund],
+      sourceFunds: [thirdPillarFund, secondPillarFund, anotherThirdPillarFund],
     });
 
     expect(state).toEqual({
       ...oldState,
-      sourceFunds: [thirdPillarFund, anotherThirdPillarFund, zeroThirdPillarFund],
+      sourceFunds: [thirdPillarFund, anotherThirdPillarFund],
       exchangeExistingUnits: true,
-      exchangeableSourceFunds: [thirdPillarFund],
+      exchangeableSourceFunds: [{ isin: 'EE123', pillar: 3 }],
     });
   });
 
@@ -149,9 +148,9 @@ describe('Third pillar reducer', () => {
       exchangeExistingUnits: true,
       selectedFutureContributionsFundIsin: 'EE789',
     };
-    const secondPillarFund = { isin: 'EE123', pillar: 2, value: 100 };
-    const anotherSecondPillarFund = { isin: 'EE456', pillar: 2, value: 100 };
-    const thirdPillarFund = { isin: 'EE789', pillar: 3, value: 100 };
+    const secondPillarFund = { isin: 'EE123', pillar: 2 };
+    const anotherSecondPillarFund = { isin: 'EE456', pillar: 2 };
+    const thirdPillarFund = { isin: 'EE789', pillar: 3 };
     const sourceFunds = [secondPillarFund, anotherSecondPillarFund, thirdPillarFund];
 
     const state = reducer(oldState, { type: GET_SOURCE_FUNDS_SUCCESS, sourceFunds });
