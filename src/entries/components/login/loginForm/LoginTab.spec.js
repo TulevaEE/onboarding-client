6fc9c77f9686a8389fdad36954910621a1ec8806
Saar <risto.saar@gmail.com>
2020-04-07 23:40:28 +0900

    Fix tab width on devices >480px <768px (#385)
    * login page fix for > 480px and < 768px screens

--- src/entries/components/login/loginForm/LoginTab.spec.js
+++ src/entries/components/login/loginForm/LoginTab.spec.js
@@ -5,12 +5,23 @@ import LoginTab from './LoginTab';
 describe('Login Tab', () => {
   it('should contain active button', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(<LoginTab label="Smart ID" activeTab="Smart ID" onClick={onClick} />);
+    const wrapper = shallow(
+      <LoginTab label="Smart ID" hideOnMobile="false" activeTab="Smart ID" onClick={onClick} />,
+    );
     expect(wrapper.find('.tab-list-item').hasClass('tab-list-active')).toBe(true);
   });
   it('should contain inactive button', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(<LoginTab label="Mobile ID" activeTab="Smart ID" onClick={onClick} />);
+    const wrapper = shallow(
+      <LoginTab label="Mobile ID" hideOnMobile="false" activeTab="Smart ID" onClick={onClick} />,
+    );
     expect(wrapper.find('.tab-list-item').hasClass('tab-list-active')).toBe(false);
+  });
+  it('should contain hidden button', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <LoginTab label="Smart ID" hideOnMobile="true" activeTab="Smart ID" onClick={onClick} />,
+    );
+    expect(wrapper.find('.tab-list-item').hasClass('d-md-table-cell')).toBe(true);
   });
 });
