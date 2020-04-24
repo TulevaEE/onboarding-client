39fa39b3d2b6e84ead0731f092ff960d28785e41
Erko Risthein <erko@risthein.ee>
2020-04-24 13:00:04 +0300

    Proper exception handling for mobile id and smart id

--- src/entries/components/common/errorMessage/ErrorMessage.js
+++ src/entries/components/common/errorMessage/ErrorMessage.js
@@ -62,7 +62,10 @@ ErrorMessage.propTypes = {
 };
 
 export function getGlobalErrorCode(errors) {
-  return (((errors || {}).errors || []).find(error => !error.path) || {}).code;
+  const globalErrorCode = (((errors || {}).errors || []).find(error => !error.path) || {}).code;
+  const firstLocalErrorCode = (((errors || {}).errors || [])[0] || {}).code;
+
+  return globalErrorCode || firstLocalErrorCode;
 }
 
 export default ErrorMessage;
