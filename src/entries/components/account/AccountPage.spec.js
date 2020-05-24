f14cbb0f1bd9a29ae334b5314c90d6aa908c007f
zhao songxun <songxunzhao1991@gmail.com>
2020-05-24 18:28:54 +0800

    Add account summary table

--- src/entries/components/account/AccountPage.spec.js
+++ src/entries/components/account/AccountPage.spec.js
@@ -7,6 +7,7 @@ import { AccountPage } from './AccountPage';
 import PendingExchangesTable from './pendingExchangeTable';
 import AccountStatement from './AccountStatement';
 import GreetingBar from './GreetingBar';
+import AccountSummary from './AccountSummary';
 
 describe('Current balance', () => {
   let component;
@@ -92,10 +93,18 @@ describe('Current balance', () => {
     it('renders the 2nd and 3rd pillar account statements', () => {
       expect(accountStatement()).toHaveLength(2);
     });
+    it('renders account summary table', () => {
+      expect(accountSummary()).toHaveLength(1);
+    });
   });
 
   it('does not render any account statements when there are no source funds', () => {
     expect(accountStatement().exists()).toBe(false);
+  });
+
+  it('does not render any account summary table when there are no source funds', () => {
+    component.setProps({ secondPillarSourceFunds: null, thirdPillarSourceFunds: null });
+    expect(accountSummary().exists()).toBe(false);
   });
 
   it('renders no second pillar message', () => {
@@ -149,5 +158,9 @@ describe('Current balance', () => {
 
   function accountStatement() {
     return component.find(AccountStatement);
+  }
+
+  function accountSummary() {
+    return component.find(AccountSummary);
   }
 });
