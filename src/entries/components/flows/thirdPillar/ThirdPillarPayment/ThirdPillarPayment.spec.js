569a26f77afa8eb6a0671fc7710a2844e1343025
aleksandraisabel <43557218+aleksandraisabel@users.noreply.github.com>
2020-07-21 11:28:58 +0300

    Third pillar payment form removal (#440)
    * Fix account summary heading
    
    * Change MARKET values and syntax to UNION_STOCK_INDEX
    
    * Remove payment section after having submitted third pillar application
    
    * Add button to redirect after payment to account
    
    * Add test
    
    Co-authored-by: Aleksandra Isabel Pillmann <aleksandra.pillmann@kcl.ac.uk>

--- src/entries/components/flows/thirdPillar/ThirdPillarPayment/ThirdPillarPayment.spec.js
+++ src/entries/components/flows/thirdPillar/ThirdPillarPayment/ThirdPillarPayment.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
+import { Message } from 'retranslate';
 import { ThirdPillarPayment } from './ThirdPillarPayment';
 
 describe('ThirdPillarPayment', () => {
@@ -26,68 +27,16 @@ describe('ThirdPillarPayment', () => {
     expect(pensionAccountNumber().text()).toBe('987');
   });
 
-  it('has monthly contribution', () => {
-    const value = () => contributionInput().prop('value');
-
-    expect(value()).toBe('');
-    component.setProps({ monthlyContribution: 1 });
-    expect(value()).toBe(1);
+  it('links to next path', () => {
+    component.setProps({ nextPath: '/next-path' });
+    expect(
+      component.contains(
+        <Link to="/next-path">
+          <button type="button" className="btn btn-primary mt-4">
+            <Message>thirdPillarPayment.paymentButton</Message>
+          </button>
+        </Link>,
+      ),
+    ).toBe(true);
   });
-
-  it('calls change handler on monthly contribution change', () => {
-    const onMonthlyContributionChange = jest.fn();
-    component.setProps({ onMonthlyContributionChange });
-
-    expect(onMonthlyContributionChange).not.toBeCalled();
-    contributionInput().simulate('change', { target: { value: '1' } });
-    expect(onMonthlyContributionChange).toBeCalledWith(1);
-  });
-
-  it('disables buttons when no monthly contribution', () => {
-    const firstButtonIsDisabled = () =>
-      component
-        .find('button')
-        .first()
-        .prop('disabled');
-
-    const secondButtonIsDisabled = () =>
-      component
-        .find('button')
-        .last()
-        .prop('disabled');
-
-    expect(firstButtonIsDisabled()).toBe(true);
-    expect(secondButtonIsDisabled()).toBe(true);
-    component.setProps({ monthlyContribution: 1 });
-    expect(firstButtonIsDisabled()).toBe(false);
-    expect(secondButtonIsDisabled()).toBe(false);
-  });
-
-  it('posts third pillar statistics on button clicks', () => {
-    const onSubmit = jest.fn();
-    const signedMandateId = 654;
-    const monthlyContribution = 100;
-
-    component.setProps({
-      signedMandateId,
-      monthlyContribution,
-      onSubmit,
-    });
-
-    singlePaymentButton().simulate('click');
-    expect(onSubmit).toBeCalledWith({
-      mandateId: signedMandateId,
-      singlePayment: monthlyContribution,
-    });
-
-    recurringPaymentButton().simulate('click');
-    expect(onSubmit).toBeCalledWith({
-      mandateId: signedMandateId,
-      recurringPayment: monthlyContribution,
-    });
-  });
-
-  const contributionInput = () => component.find('#monthly-contribution');
-  const recurringPaymentButton = () => component.find('button').at(0);
-  const singlePaymentButton = () => component.find('button').at(1);
 });
