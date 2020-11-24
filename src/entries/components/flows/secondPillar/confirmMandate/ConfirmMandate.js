b8a72a72470250053a0dde41794d8c82ea490d09
Tõnu Pekk <tonu.pekk@tuleva.ee>
2020-11-24 12:15:00 +0200

    fix broken link

--- src/entries/components/flows/secondPillar/confirmMandate/ConfirmMandate.js
+++ src/entries/components/flows/secondPillar/confirmMandate/ConfirmMandate.js
@@ -178,7 +178,7 @@ export const ConfirmMandate = ({
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="//www.pensionikeskus.ee/ii-sammas/fondid/fonditasude-vordlused/"
+                  href="//www.pensionikeskus.ee/ii-sammas/fondid/fondide-tasud/fonditasude-vordlused/"
                 >
                   <Message>confirm.mandate.pension.centre</Message>
                 </a>
