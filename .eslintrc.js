2801479b53188955c1540faf2652363675de1fdd
Erko Risthein <erko@risthein.ee>
2021-02-18 15:29:58 +0200

    Add todo about eslint rule activation

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     ], // for Enzyme
     '@typescript-eslint/no-use-before-define': 0, // for clean code
     '@typescript-eslint/explicit-member-accessibility': 0, // too verbose for React components
-    '@typescript-eslint/explicit-function-return-type': 0, // to fix build after ESLint upgrade
+    '@typescript-eslint/explicit-function-return-type': 0, // to fix build after ESLint upgrade. TODO: activate
     'react/state-in-constructor': [1, 'never'],
     'react/prop-types': 0, // TypeScript does it for us
     '@typescript-eslint/no-empty-function': 0, // we use it
