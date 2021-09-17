1fda28bbae1dd0c5d2d8e5c978f301c154f895f4
Mats Johanson <mats@johanson.ee>
2021-09-17 11:15:33 +0300

    Add submitting state to user contacts reducer

--- src/components/contact-details/reducer.js
+++ src/components/contact-details/reducer.js
@@ -1,20 +1,28 @@
-import { UPDATE_USER_SUCCESS, USER_UPDATED } from '../common/user/constants';
+import { UPDATE_USER_START, UPDATE_USER_SUCCESS, USER_UPDATED } from '../common/user/constants';
 
 export const initialState = {
   updateUserSuccess: false,
+  submitting: false,
 };
 
 export default function accountReducer(state = initialState, action) {
   switch (action.type) {
+    case UPDATE_USER_START:
+      return {
+        ...state,
+        submitting: true,
+      };
     case UPDATE_USER_SUCCESS:
       return {
         ...state,
         updateUserSuccess: true,
+        submitting: false,
       };
     case USER_UPDATED:
       return {
         ...state,
         updateUserSuccess: false,
+        submitting: false,
       };
     default:
       return state;
