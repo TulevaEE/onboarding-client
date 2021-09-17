bf338ea63cce4d70ce898765c3f576e732262771
Mats Johanson <mats@johanson.ee>
2021-09-17 11:15:33 +0300

    Add submitting state to user contacts reducer

--- src/components/contact-details/reducer.spec.js
+++ src/components/contact-details/reducer.spec.js
@@ -1,21 +1,35 @@
-import { UPDATE_USER_SUCCESS, USER_UPDATED } from '../common/user/constants';
+import { UPDATE_USER_START, UPDATE_USER_SUCCESS, USER_UPDATED } from '../common/user/constants';
 
 import accountReducer from './reducer';
 
 describe('Contact detail reducer', () => {
+  it('sets submitting state when starting user update', () => {
+    const action = { type: UPDATE_USER_START };
+    const newState = accountReducer({ submitting: false }, action);
+    expect(newState.submitting).toBe(true);
+  });
+
+  it('sets submitting state when update finished', () => {
+    const action = { type: USER_UPDATED };
+    const newState = accountReducer({ submitting: true }, action);
+    expect(newState.submitting).toBe(false);
+  });
+
+  it('sets submitting state when update success', () => {
+    const action = { type: UPDATE_USER_SUCCESS };
+    const newState = accountReducer({ submitting: true }, action);
+    expect(newState.submitting).toBe(false);
+  });
+
   it('correctly sets user update success to state', () => {
     const action = { type: UPDATE_USER_SUCCESS };
-
     const newState = accountReducer({ updateUserSuccess: false }, action);
-
     expect(newState.updateUserSuccess).toBe(true);
   });
 
   it('removes user update success from state', () => {
     const action = { type: USER_UPDATED };
-
     const newState = accountReducer({ updateUserSuccess: true }, action);
-
     expect(newState.updateUserSuccess).toBe(false);
   });
 });
