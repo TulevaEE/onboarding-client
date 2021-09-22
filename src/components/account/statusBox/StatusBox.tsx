0b68c2eb52d6b2901ad9052572b0e39201d16969
Mats Johanson <mats@johanson.ee>
2021-09-22 15:49:15 +0300

    Split member status box into its own component

--- src/components/account/statusBox/StatusBox.tsx
+++ src/components/account/statusBox/StatusBox.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Message } from 'retranslate';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { StatusBoxRow } from './statusBoxRow/StatusBoxRow';
 import SecondPillarStatusBox from './secondPillarStatusBox';
 import { Fund, UserConversion } from './types';
 import { StatusBoxLoader } from './StatusBoxLoader';
 import { StatusBoxTitle } from './StatusBoxTitle';
 import ThirdPillarStatusBox from './thirdPillarStatusBox';
+import MemberStatusBox from './memberStatusBox';
 
 interface StatusBoxType {
-  memberNumber: number | null;
   conversion?: UserConversion;
-  loading: boolean;
   secondPillarFunds: Fund[];
   thirdPillarFunds: Fund[];
 }
 
 export const StatusBox: React.FunctionComponent<StatusBoxType> = ({
   conversion,
-  memberNumber = null,
-  loading = false,
   secondPillarFunds = [],
   thirdPillarFunds = [],
 }) => {
   if (!conversion || !secondPillarFunds || !thirdPillarFunds) {
     return <StatusBoxLoader />;
   }
-  const isTulevaMember = memberNumber != null;
-  const tulevaData = isTulevaMember
-    ? [<Message params={{ memberNumber }}>account.member.statement</Message>]
-    : [<Message>account.non.member.statement</Message>];
-
   return (
     <>
       <StatusBoxTitle />
@@ -39,21 +29,9 @@ export const StatusBox: React.FunctionComponent<StatusBoxType> = ({
       <div className="card card-secondary">
         <SecondPillarStatusBox />
         <ThirdPillarStatusBox />
-
-        <StatusBoxRow
-          last
-          ok={isTulevaMember}
-          showAction={!loading}
-          name={<Message>account.status.choice.tuleva</Message>}
-          lines={tulevaData}
-        >
-          {!isTulevaMember && (
-            <Link to="/join" className="btn btn-light">
-              <Message>account.status.choice.join.tuleva</Message>
-            </Link>
-          )}
-        </StatusBoxRow>
+        <MemberStatusBox />
       </div>
+
       <div className="mt-3">
         <small className="text-muted">
           <Message
@@ -71,15 +49,11 @@ export const StatusBox: React.FunctionComponent<StatusBoxType> = ({
 const mapStateToProps = (state: {
   login: {
     userConversion: UserConversion;
-    loadingUserConversion: boolean;
-    user: { memberNumber: number };
   };
   exchange: { sourceFunds: Fund[] };
   thirdPillar: { sourceFunds: Fund[] };
 }) => ({
-  memberNumber: (state.login.user || {}).memberNumber,
   conversion: state.login.userConversion,
-  loading: state.login.loadingUserConversion,
   secondPillarFunds: state.exchange.sourceFunds,
   thirdPillarFunds: state.thirdPillar.sourceFunds,
 });
