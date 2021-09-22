807f4bdf92276b671245ea0af9af76044a9ac3b1
Mats Johanson <mats@johanson.ee>
2021-09-22 15:49:15 +0300

    Split member status box into its own component

---
+++ src/components/account/statusBox/memberStatusBox/MemberStatusBox.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Message } from 'retranslate';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { StatusBoxRow } from '../statusBoxRow/StatusBoxRow';
+
+interface Props {
+  loading: boolean;
+  memberNumber: number;
+}
+
+export const MemberStatusBox: React.FunctionComponent<Props> = ({
+  loading = false,
+  memberNumber,
+}) => {
+  const isTulevaMember = memberNumber != null;
+  const tulevaData = isTulevaMember
+    ? [<Message params={{ memberNumber }}>account.member.statement</Message>]
+    : [<Message>account.non.member.statement</Message>];
+
+  return (
+    <StatusBoxRow
+      last
+      ok={isTulevaMember}
+      showAction={!loading}
+      name={<Message>account.status.choice.tuleva</Message>}
+      lines={tulevaData}
+    >
+      {!isTulevaMember && (
+        <Link to="/join" className="btn btn-light">
+          <Message>account.status.choice.join.tuleva</Message>
+        </Link>
+      )}
+    </StatusBoxRow>
+  );
+};
+
+const mapStateToProps = (state: {
+  login: {
+    loadingUserConversion: boolean;
+    user: { memberNumber: number };
+  };
+}) => ({
+  memberNumber: (state.login.user || {}).memberNumber,
+  loading: state.login.loadingUserConversion,
+});
+
+export default connect(mapStateToProps)(MemberStatusBox);
