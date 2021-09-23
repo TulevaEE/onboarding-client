fc8e1a06ce9be4a9416c28b8d5514b5fd89aa7e5
Mats Johanson <mats@johanson.ee>
2021-09-23 13:53:25 +0300

    Clean up status box test

--- src/components/account/statusBox/StatusBox.test.tsx
+++ src/components/account/statusBox/StatusBox.test.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import { StatusBox } from './StatusBox';
-import { SourceFund } from '../../common/apiModels';
-
-jest.unmock('retranslate');
+import { completeConversion } from './fixtures';
 
 describe('Status Box', () => {
-  const conversion = {
-    secondPillar: {
-      selectionComplete: false,
-      transfersComplete: false,
-      paymentComplete: false,
-      pendingWithdrawal: false,
-      contribution: { yearToDate: 200, total: 250 },
-      subtraction: { yearToDate: 0, total: 0 },
-    },
-    thirdPillar: {
-      selectionComplete: false,
-      transfersComplete: false,
-      paymentComplete: false,
-      pendingWithdrawal: false,
-      contribution: { yearToDate: 300, total: 450 },
-      subtraction: { yearToDate: 0, total: 0 },
-    },
-  };
-  const secondPillarFunds: SourceFund[] = [];
-  const thirdPillarFunds: SourceFund[] = [];
-  const props = { conversion, secondPillarFunds, thirdPillarFunds };
-  const component = shallow(<StatusBox {...props} />);
+  let component: ShallowWrapper;
+  const props = { conversion: completeConversion, secondPillarFunds: [], thirdPillarFunds: [] };
+
+  beforeEach(() => {
+    component = shallow(<StatusBox {...props} />);
+  });
 
   it('renders all subcomponents when present', () => {
     expect(component).toMatchSnapshot();
