619d775b05d002f758d9f4d88b1692e1e6b409c8
Mats Johanson <mats@johanson.ee>
2022-01-20 15:13:02 +0200

    Switch to react-intl in login-form

--- src/components/login/loginForm/LoginForm.js
+++ src/components/login/loginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PropTypes as Types } from 'prop-types';
-import { Message, useTranslations } from 'retranslate';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import './LoginForm.scss';
 import LoginTabs from './LoginTabs';
@@ -62,37 +62,50 @@ const renderLoginForm = (
   phoneNumber,
   onPhoneNumberChange,
   onAuthenticateWithIdCard,
-) => (
-  <>
-    <div className="col-lg-9">
-      <div className="mt-2 mb-4">
-        {monthlyThirdPillarContribution ? (
-          renderMonthlyThirdPillarNotice(
-            exchangeExistingThirdPillarUnits,
-            monthlyThirdPillarContribution,
-          )
-        ) : (
-          <h3>
-            <Message>login.title</Message>
-          </h3>
+) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <>
+      <div className="col-lg-9">
+        <div className="mt-2 mb-4">
+          {monthlyThirdPillarContribution ? (
+            renderMonthlyThirdPillarNotice(
+              exchangeExistingThirdPillarUnits,
+              monthlyThirdPillarContribution,
+            )
+          ) : (
+            <h3>
+              <FormattedMessage id="login.title" />
+            </h3>
+          )}
+        </div>
+
+        {renderLoginTabs(
+          onIdCodeSubmit,
+          personalCode,
+          onPersonalCodeChange,
+          onMobileIdSubmit,
+          phoneNumber,
+          onPhoneNumberChange,
+          onAuthenticateWithIdCard,
+          formatMessage,
         )}
       </div>
 
-      {renderLoginTabs(
-        onIdCodeSubmit,
-        personalCode,
-        onPersonalCodeChange,
-        onMobileIdSubmit,
-        phoneNumber,
-        onPhoneNumberChange,
-        onAuthenticateWithIdCard,
-      )}
-    </div>
-    <div className="col-lg-9 mt-4">
-      <Message dangerouslyTranslateInnerHTML="login.permission.note" />
-    </div>
-  </>
-);
+      <div className="col-lg-9 mt-4">
+        <FormattedMessage
+          id="login.permission.note"
+          values={{
+            a: (chunks) => (
+              <a href={formatMessage({ id: 'login.permission.note.url' })}>{chunks}</a>
+            ),
+          }}
+        />
+      </div>
+    </>
+  );
+};
 
 const renderMonthlyThirdPillarNotice = (
   exchangeExistingThirdPillarUnits,
@@ -101,26 +114,20 @@ const renderMonthlyThirdPillarNotice = (
   <>
     <h3 className="mb-4">
       {exchangeExistingThirdPillarUnits ? (
-        <Message
-          params={{
-            monthlyContribution: monthlyThirdPillarContribution,
-          }}
-        >
-          login.title.thirdPillar.withExchange
-        </Message>
+        <FormattedMessage
+          id="login.title.thirdPillar.withExchange"
+          values={{ monthlyContribution: monthlyThirdPillarContribution }}
+        />
       ) : (
-        <Message
-          params={{
-            monthlyContribution: monthlyThirdPillarContribution,
-          }}
-        >
-          login.title.thirdPillar.withoutExchange
-        </Message>
+        <FormattedMessage
+          id="login.title.thirdPillar.withoutExchange"
+          values={{ monthlyContribution: monthlyThirdPillarContribution }}
+        />
       )}
     </h3>
 
     <h3>
-      <Message>login.subtitle.thirdPillar</Message>
+      <FormattedMessage id="login.subtitle.thirdPillar" />
     </h3>
   </>
 );
@@ -133,26 +140,23 @@ const renderLoginTabs = (
   phoneNumber,
   onPhoneNumberChange,
   onAuthenticateWithIdCard,
-) => {
-  const { translate } = useTranslations();
+  formatMessage,
+) => (
+  <LoginTabs>
+    {renderSmartId(onIdCodeSubmit, personalCode, onPersonalCodeChange, formatMessage)}
+    {renderMobileId(
+      onMobileIdSubmit,
+      phoneNumber,
+      personalCode,
+      onPersonalCodeChange,
+      formatMessage,
+      onPhoneNumberChange,
+    )}
+    {renderIdCard(onAuthenticateWithIdCard)}
+  </LoginTabs>
+);
 
-  return (
-    <LoginTabs>
-      {renderSmartId(onIdCodeSubmit, personalCode, onPersonalCodeChange, translate)}
-      {renderMobileId(
-        onMobileIdSubmit,
-        phoneNumber,
-        personalCode,
-        onPersonalCodeChange,
-        translate,
-        onPhoneNumberChange,
-      )}
-      {renderIdCard(onAuthenticateWithIdCard)}
-    </LoginTabs>
-  );
-};
-
-const renderSmartId = (onIdCodeSubmit, personalCode, onPersonalCodeChange, translate) => (
+const renderSmartId = (onIdCodeSubmit, personalCode, onPersonalCodeChange, formatMessage) => (
   <div label="login.smart.id" hideOnMobile="false">
     <form onSubmit={runWithDefaultPrevention(() => onIdCodeSubmit(personalCode))}>
       <div className="form-group">
@@ -162,7 +166,7 @@ const renderSmartId = (onIdCodeSubmit, personalCode, onPersonalCodeChange, trans
           value={personalCode}
           onChange={(event) => onPersonalCodeChange(event.target.value)}
           className="form-control form-control-lg"
-          placeholder={translate('login.id.code')}
+          placeholder={formatMessage({ id: 'login.id.code' })}
         />
       </div>
       <div className="form-group">
@@ -171,7 +175,7 @@ const renderSmartId = (onIdCodeSubmit, personalCode, onPersonalCodeChange, trans
           type="submit"
           className="btn btn-primary btn-block btn-lg"
           disabled={!personalCode}
-          value={translate('login.enter')}
+          value={formatMessage({ id: 'login.enter' })}
         />
       </div>
     </form>
@@ -183,7 +187,7 @@ const renderMobileId = (
   phoneNumber,
   personalCode,
   onPersonalCodeChange,
-  translate,
+  formatMessage,
   onPhoneNumberChange,
 ) => (
   <div label="login.mobile.id" hideOnMobile="false">
@@ -195,7 +199,7 @@ const renderMobileId = (
           value={personalCode}
           onChange={(event) => onPersonalCodeChange(event.target.value)}
           className="form-control form-control-lg"
-          placeholder={translate('login.id.code')}
+          placeholder={formatMessage({ id: 'login.id.code' })}
         />
       </div>
       <div className="form-group">
@@ -205,7 +209,7 @@ const renderMobileId = (
           value={phoneNumber}
           onChange={(event) => onPhoneNumberChange(event.target.value)}
           className="form-control form-control-lg"
-          placeholder={translate('login.phone.number')}
+          placeholder={formatMessage({ id: 'login.phone.number' })}
         />
       </div>
       <div className="form-group">
@@ -214,7 +218,7 @@ const renderMobileId = (
           type="submit"
           className="btn btn-primary btn-block btn-lg"
           disabled={!phoneNumber || !personalCode}
-          value={translate('login.enter')}
+          value={formatMessage({ id: 'login.enter' })}
         />
       </div>
     </form>
@@ -229,7 +233,7 @@ const renderIdCard = (onAuthenticateWithIdCard) => (
         className="btn btn-primary btn-block btn-lg"
         onClick={onAuthenticateWithIdCard}
       >
-        <Message>login.enter</Message>
+        <FormattedMessage>login.enter</FormattedMessage>
       </button>
     </div>
   </div>
