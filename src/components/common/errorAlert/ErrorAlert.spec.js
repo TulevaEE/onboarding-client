b5cf6afbf3af51e6d98f2829e460c44c30fb3b98
Mats Johanson <mats@johanson.ee>
2022-02-08 10:39:48 +0200

    Convert error alert to react-intl

--- src/components/common/errorAlert/ErrorAlert.spec.js
+++ src/components/common/errorAlert/ErrorAlert.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Message } from 'retranslate';
 import { shallow } from 'enzyme';
 
+import { FormattedMessage } from 'react-intl';
 import ErrorAlert from './ErrorAlert';
 
 describe('Error alert', () => {
@@ -13,17 +13,17 @@ describe('Error alert', () => {
 
   it('displays a generic message for an unknown error', () => {
     component.setProps({ description: 'oh man something is wrong!' });
-    expect(component.contains(<Message>login.error.generic</Message>)).toBe(true);
+    expect(component.contains(<FormattedMessage id="login.error.generic" />)).toBe(true);
   });
 
   it('does not display a generic message when user has not joined tuleva', () => {
     component.setProps({ description: 'INVALID_USER_CREDENTIALS' });
-    expect(component.contains(<Message>login.error.generic</Message>)).toBe(false);
+    expect(component.contains(<FormattedMessage id="login.error.generic" />)).toBe(false);
   });
 
   it('does not display a generic message when invalid personal code was provided', () => {
     component.setProps({ description: 'ValidPersonalCode' });
-    expect(component.contains(<Message>login.error.generic</Message>)).toBe(false);
+    expect(component.contains(<FormattedMessage id="login.error.generic" />)).toBe(false);
   });
 
   it('shows a call to action with a link to join tuleva when user has not joined tuleva', () => {
@@ -31,15 +31,17 @@ describe('Error alert', () => {
     expect(
       component.contains(
         <a href="//tuleva.ee/#liitu">
-          <Message>login.join.tuleva</Message>
+          <FormattedMessage id="login.join.tuleva" />
         </a>,
       ),
     ).toBe(true);
-    expect(component.contains(<Message>login.error.invalid.user.credentials</Message>)).toBe(true);
+    expect(component.contains(<FormattedMessage id="login.error.invalid.user.credentials" />)).toBe(
+      true,
+    );
   });
 
   it('shows id card login start failed error message', () => {
     component.setProps({ description: 'ID_CARD_LOGIN_START_FAILED' });
-    expect(component.contains(<Message>login.id.card.start.failed</Message>)).toBe(true);
+    expect(component.contains(<FormattedMessage id="login.id.card.start.failed" />)).toBe(true);
   });
 });
