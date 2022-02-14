6d86a6a5399fb3389eef26b9828bfac90baeddfa
Mats Johanson <mats@johanson.ee>
2022-02-14 15:05:48 +0200

    Convert second pillar flow to react-intl

--- src/components/flows/secondPillar/SecondPillarFlow.js
+++ src/components/flows/secondPillar/SecondPillarFlow.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Message } from 'retranslate';
 
+import { FormattedMessage } from 'react-intl';
 import SelectSources from './selectSources';
 import TransferFutureCapital from './transferFutureCapital';
 import ConfirmMandate from './confirmMandate';
@@ -9,12 +9,12 @@ import Flow from '../common/Flow'; // eslint-disable-line import/no-named-as-def
 import AddressStep from '../common/AddressStep';
 
 const flowPath = '/2nd-pillar-flow';
-const introMessage = <Message>steps.intro</Message>;
+const introMessage = <FormattedMessage id="steps.intro" />;
 const steps = [
   {
     path: 'select-sources',
     Component: () => <SelectSources nextPath={`${flowPath}/transfer-future-capital`} />,
-    title: <Message>steps.select-sources</Message>,
+    title: <FormattedMessage id="steps.select-sources" />,
   },
   {
     path: 'transfer-future-capital',
@@ -24,21 +24,21 @@ const steps = [
         nextPath={`${flowPath}/address`}
       />
     ),
-    title: <Message>steps.transfer-future-capital</Message>,
+    title: <FormattedMessage id="steps.transfer-future-capital" />,
   },
   {
     path: 'address',
     Component: () => (
       <AddressStep nextPath={`${flowPath}/confirm-mandate`} updateOnlyEmailAndPhone pillar={2} />
     ),
-    title: <Message>steps.address</Message>,
+    title: <FormattedMessage id="steps.address" />,
   },
   {
     path: 'confirm-mandate',
     Component: () => (
       <ConfirmMandate previousPath={`${flowPath}/address`} nextPath={`${flowPath}/success`} />
     ),
-    title: <Message>steps.confirm-mandate</Message>,
+    title: <FormattedMessage id="steps.confirm-mandate" />,
   },
   {
     path: 'success',
