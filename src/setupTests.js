eff54ddb75ea0ed573ac9737af160bda99918ca7
Mats Johanson <mats@johanson.ee>
2022-02-14 18:02:38 +0200

    Remove retranslate

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,6 @@ import { configure } from 'enzyme'; // eslint-disable-line import/no-extraneous-
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // eslint-disable-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom'; // eslint-disable-line import/no-extraneous-dependencies
 
-// necessary due to https://github.com/facebook/create-react-app/issues/7539
-jest.mock('retranslate', () => jest.requireActual('../__mocks__/retranslate'));
-
 jest.mock('react-intl', () => {
   const reactIntl = jest.requireActual('react-intl');
   const intl = reactIntl.createIntl({
