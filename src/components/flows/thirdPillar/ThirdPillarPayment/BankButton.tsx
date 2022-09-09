815b8dd9089569f90c25b9295084e5f5b3e150c1
Erko Risthein <erko@producement.com>
2022-09-09 12:36:48 +0300

    Fix bank button designs, add typescript types, extract BankButton component

---
+++ src/components/flows/thirdPillar/ThirdPillarPayment/BankButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const BankButton: React.FunctionComponent<{
+  bankKey: string;
+  bankName: string;
+  paymentBank: string;
+  setPaymentBank: (paymentBank: string) => void;
+}> = ({ bankKey, bankName, paymentBank, setPaymentBank }) => {
+  return (
+    <div className="btn-group-toggle d-inline-block mt-2 mr-2">
+      <label
+        className={`btn btn-light btn-payment btn-${bankKey} text-nowrap ${
+          paymentBank === bankKey ? 'active' : ''
+        }`}
+      >
+        <input
+          type="radio"
+          name="banks"
+          id={bankKey}
+          checked={paymentBank === bankKey}
+          onChange={() => {
+            setPaymentBank(bankKey);
+          }}
+        />
+        {bankName}
+      </label>
+    </div>
+  );
+};
