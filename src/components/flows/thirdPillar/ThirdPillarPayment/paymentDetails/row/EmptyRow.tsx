4a3fc7e6ba14b4278bf17b1902507e17845bf3a0
Erko Risthein <erko@producement.com>
2022-10-21 15:36:26 +0300

    ESLint fixes

--- src/components/flows/thirdPillar/ThirdPillarPayment/paymentDetails/row/EmptyRow.tsx
+++ src/components/flows/thirdPillar/ThirdPillarPayment/paymentDetails/row/EmptyRow.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export function EmptyRow(): React.ReactNode {
-  return (
-    <tr>
-      <td colSpan={3}>&nbsp;</td>
-    </tr>
-  );
-}
+export const EmptyRow: React.FunctionComponent = () => (
+  <tr>
+    <td colSpan={3}>&nbsp;</td>
+  </tr>
+);
