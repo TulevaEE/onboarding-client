6c5f7b49c379a18c8a82d4e19aa543e037bf7231
Maido Käära <maido@producement.com>
2022-10-31 12:17:24 +0200

    Set Sentry sample rate to 1.0

--- src/polyfills.js
+++ src/polyfills.js
@@ -10,6 +10,7 @@ if (isProduction) {
   Sentry.init({
     dsn: 'https://cfcb0c4bb8cb4264942f80ca1eb78c49@sentry.io/146907',
     environment: process.env.NODE_ENV,
+    sampleRate: 1.0,
   });
 
   // Rejection tracking prevents a common issue where React gets into an
