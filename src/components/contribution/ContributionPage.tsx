a8a58a70f008d2b11be7f8647ce2f8fcdc409880
Erko Risthein <erko@producement.com>
2023-07-19 14:48:40 +0300

    Aggregate contributions

--- src/components/contribution/ContributionPage.tsx
+++ src/components/contribution/ContributionPage.tsx
@@ -5,11 +5,8 @@ import { ContributionSection } from './ContributionSection';
 export const ContributionPage: React.FunctionComponent = () => {
   return (
     <>
-      <ContributionSection pillar={2}>
-        <FormattedMessage id="contributions.title.2nd" />
-      </ContributionSection>
-      <ContributionSection pillar={3}>
-        <FormattedMessage id="contributions.title.3rd" />
+      <ContributionSection>
+        <FormattedMessage id="contributions.title" />
       </ContributionSection>
     </>
   );
--- src/components/contribution/ContributionSection.tsx
+++ src/components/contribution/ContributionSection.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
-import moment from 'moment/moment';
 import sumBy from 'lodash/sumBy';
 import { useContributions } from '../common/apiHooks';
 import { Shimmer } from '../common/shimmer/Shimmer';
 import Euro from '../common/Euro';
 import Table from '../common/table';
+import { Contribution } from '../common/apiModels';
 
 export const ContributionSection: React.FunctionComponent<{
-  limit?: number;
-  pillar?: number;
   children?: React.ReactNode;
-}> = ({ limit, pillar, children }) => {
+}> = ({ children }) => {
   const { data: contributions } = useContributions();
 
   if (!contributions) {
@@ -28,25 +25,34 @@ export const ContributionSection: React.FunctionComponent<{
     return <></>;
   }
 
-  let pillarContributions = contributions;
+  const amountSum = sumBy(contributions, (contribution) => contribution.amount);
 
-  if (pillar) {
-    pillarContributions = contributions.filter((contribution) => contribution.pillar === pillar);
-  }
+  const sumAmountsBySender = (
+    result: {
+      [sender: string]: Contribution;
+    },
+    { time, sender, amount, currency, pillar }: Contribution,
+  ) => ({
+    ...result,
+    [sender.toLowerCase()]: {
+      time,
+      sender,
+      amount: (result[sender.toLowerCase()] || { amount: 0 }).amount + amount,
+      currency,
+      pillar,
+    },
+  });
+  const thirdPillarAggregate = contributions
+    .filter((contribution) => contribution.pillar === 3)
+    .sort((contribution1, contribution2) => contribution2.time.localeCompare(contribution1.time))
+    .reduce(sumAmountsBySender, {});
 
-  if (!pillarContributions.length) {
-    return <></>;
-  }
-
-  const amountSum = sumBy(pillarContributions, (contribution) => contribution.amount);
+  const secondPillarAggregate = contributions
+    .filter((contribution) => contribution.pillar === 2)
+    .sort((contribution1, contribution2) => contribution2.time.localeCompare(contribution1.time))
+    .reduce(sumAmountsBySender, {});
 
   const columns = [
-    {
-      title: <FormattedMessage id="contributions.columns.date.title" />,
-      dataIndex: 'date',
-      align: 'right',
-      ...(!limit && { footer: <FormattedMessage id="contributions.columns.date.footer" /> }),
-    },
     {
       title: <FormattedMessage id="contributions.columns.sender.title" />,
       dataIndex: 'sender',
@@ -56,61 +62,47 @@ export const ContributionSection: React.FunctionComponent<{
     {
       title: <FormattedMessage id="contributions.columns.amount.title" />,
       dataIndex: 'amount',
-      ...(!limit && { footer: <Euro amount={amountSum} /> }),
+      footer: <Euro amount={amountSum} />,
     },
   ];
 
-  let dataSource = pillarContributions
-    .sort((contribution1, contribution2) => contribution2.time.localeCompare(contribution1.time))
-    .flatMap((contribution, index) => {
-      const previousContribution = pillarContributions[index - 1];
-      const previousYear = new Date(previousContribution?.time || '9999-12-31').getUTCFullYear();
-      const currentYear = new Date(contribution.time).getUTCFullYear();
-      const date = formatDate(contribution.time);
-      return [
-        ...(!limit && previousYear > currentYear
-          ? [
-              {
-                date: <strong>{currentYear}</strong>,
-                key: currentYear.toString(),
-              },
-            ]
-          : []),
-        {
-          date: <span className="text-nowrap">{date}</span>,
-          sender: <span>{contribution.sender}</span>,
-          amount: <Euro amount={contribution.amount} />,
-          key: contribution.time,
-        },
-      ];
-    });
-
-  if (limit) {
-    dataSource = dataSource.slice(0, limit);
-  }
+  const pillarContributions = [
+    ...Object.values(thirdPillarAggregate),
+    ...Object.values(secondPillarAggregate),
+  ];
+  const dataSource = pillarContributions.flatMap((contribution, index) => {
+    const previousContribution = pillarContributions[index - 1];
+    return [
+      ...(previousContribution?.pillar !== contribution.pillar
+        ? [
+            {
+              sender: (
+                <strong>
+                  {contribution.pillar === 2 ? (
+                    <FormattedMessage id="accountStatement.secondPillar.heading" />
+                  ) : (
+                    <FormattedMessage id="accountStatement.thirdPillar.heading" />
+                  )}
+                </strong>
+              ),
+              key: contribution.pillar.toString(),
+            },
+          ]
+        : []),
+      {
+        sender: contribution.sender,
+        amount: <Euro amount={contribution.amount} />,
+        key: contribution.time,
+      },
+    ];
+  });
 
   return (
     <section className="mt-5">
       <div className="d-flex justify-content-between">
         <h2 className="mb-4 lead">{children || <FormattedMessage id="contributions.title" />}</h2>
-        <div>
-          {limit ? (
-            <Link to="/contributions">
-              <FormattedMessage id="contributions.seeAll" />
-            </Link>
-          ) : (
-            <Link to="/account">
-              <FormattedMessage id="contributions.backToAccountPage" />
-            </Link>
-          )}
-        </div>
       </div>
       <Table columns={columns} dataSource={dataSource} />
     </section>
   );
 };
-
-function formatDate(date: string): string {
-  const format = moment.locale() === 'et' ? 'D. MMMM' : 'MMMM D';
-  return moment(date).format(format);
-}
