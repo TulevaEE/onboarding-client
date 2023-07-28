907d5582c152dd70441159da7f8ad6ed8ad1e114
Erko Risthein <erko@producement.com>
2023-07-28 10:00:15 +0300

    Revert "Replace GA4 with GTM"
    This reverts commit d8359629e63895daa7de5e8f83a104f457a1cf8e.

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ initializeConfiguration();
 window.config = config; // for debug only
 
 if (process.env.NODE_ENV !== 'test') {
-  ReactGA.initialize('GTM-MRRG43', {
+  ReactGA.initialize('G-2LNCGK63HR', {
     debug: false,
     titleCase: false,
     gaOptions: {
