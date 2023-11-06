a2bc205a5043934da666f1f7f1e6a625f3d227e5
Rauno Viskus <rauno56@gmail.com>
2023-11-06 14:18:38 +0200

    feat: show the loading spinner and redirect after successful exchange

--- src/components/Authenticate/Authenticate.tsx
+++ src/components/Authenticate/Authenticate.tsx
@@ -1,34 +1,58 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
+import Loader from '../common/loader';
 import { exchangeHandoverToken } from '../common/api';
 import { getQueryParams } from '../../utils';
+import { MOBILE_AUTHENTICATION_SUCCESS } from '../login/constants';
 
 export const Authenticate: React.FC = () => {
-  const { handoverToken, provider } = getQueryParams();
+  const { handoverToken, provider, redirect } = getQueryParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   React.useEffect(() => {
-    if (typeof handoverToken === 'string') {
-      console.log('do stuff', provider, handoverToken);
-    } else {
-      console.error('invalid token', handoverToken);
+    if (typeof handoverToken !== 'string') {
       throw new Error('TODO: Handle invalid token');
+      console.error('invalid token', provider, handoverToken);
     }
+    console.log('exchanging', provider, handoverToken);
     exchangeHandoverToken(handoverToken)
       .then((res) => {
+        const { accessToken } = res;
         console.log('successfully exchanged', res);
+        dispatch({
+          type: MOBILE_AUTHENTICATION_SUCCESS,
+          tokens: { accessToken },
+          method: 'handoverToken',
+          provider,
+        });
+        console.log('redirecting');
+        if (typeof redirect !== 'undefined') {
+          // using push for ease of testings for now, will be turning into replace, because hitting back should skip this component
+          history.push('/');
+          // history.replace('/');
+        }
       })
       .catch((err) => {
         console.error('error on exchange', err);
       });
   }, [handoverToken]);
 
-  // render loading screen... or?
   return (
-    <>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-      <div>Loading...</div>
-    </>
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Loader className="align-middle" />
+    </div>
   );
 };
 
