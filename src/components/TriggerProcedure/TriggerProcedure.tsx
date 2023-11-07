7cad9ab36a0fe54dbed25819614436ae5ecbb5f2
Rauno Viskus <rauno56@gmail.com>
2023-11-07 17:34:03 +0200

    feat: rename externalProviderUtils to just utils

--- src/components/TriggerProcedure/TriggerProcedure.tsx
+++ src/components/TriggerProcedure/TriggerProcedure.tsx
@@ -7,7 +7,7 @@ import Loader from '../common/loader';
 import { exchangeHandoverTokenForAccessToken } from './exchangeHandoverToken';
 import { getQueryParams } from '../../utils';
 import { MOBILE_AUTHENTICATION_SUCCESS } from '../login/constants';
-import { init } from './externalProviderUtils';
+import { init } from './utils';
 
 import './TriggerProcedure.scss';
 
