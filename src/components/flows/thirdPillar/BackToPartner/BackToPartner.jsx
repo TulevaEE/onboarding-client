064a420b48087b6306d279f1d67a7704281c51d7
Rauno Viskus <rauno56@gmail.com>
2023-11-08 15:32:03 +0200

    feat: make styles passable

--- src/components/flows/thirdPillar/BackToPartner/BackToPartner.jsx
+++ src/components/flows/thirdPillar/BackToPartner/BackToPartner.jsx
@@ -22,20 +22,26 @@ const BackToPartnerDefault = () => {
           </SuccessNotice>
         </div>
       </div>
-      <div className="row mt-8">
+      <div className="row">
         <div className="col-12 px-0">
           <Notice>
-            <h2 className="text-center mt-3">
+            <h2 className="mt-0 text-center">
               <FormattedMessage id="thirdPillarBackToPartner.automateNext" />
             </h2>
-            <p className="mt-5">
+            <p className="mt-3">
               <FormattedMessage id="thirdPillarBackToPartner.automateNext.subtitle" />
             </p>
-            <button type="button" className="btn btn-primary mt-4 profile-link" onClick={finish}>
+            <button
+              type="button"
+              className="btn btn-block d-md-inline btn-primary mt-4 profile-link"
+              onClick={finish}
+            >
               <FormattedMessage id="thirdPillarBackToPartner.recurringPayment.button" />
             </button>
-            <p className="mt-5">
-              <FormattedMessage id="thirdPillarBackToPartner.recurringPayment.subtitle" />
+            <p className="mt-2">
+              <small className="text-muted">
+                <FormattedMessage id="thirdPillarBackToPartner.recurringPayment.subtitle" />
+              </small>
             </p>
           </Notice>
         </div>
