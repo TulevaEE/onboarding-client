51c1e72272bedfc5f94ce30b4d687db6844c9bd3
Rauno Viskus <rauno56@gmail.com>
2023-11-08 15:49:08 +0200

    feat: get rid of the unneeded success logic

--- src/components/flows/thirdPillar/BackToPartner/BackToPartner.spec.jsx
+++ src/components/flows/thirdPillar/BackToPartner/BackToPartner.spec.jsx
@@ -7,59 +7,8 @@ import { BackToPartner } from './BackToPartner';
 describe('Third pillar success step', () => {
   let component;
 
-  const secondPillarSourceFundsLowFee = [
-    {
-      isin: 'EE3600109435',
-      name: 'Our Fund',
-      ongoingChargesFigure: 0.004,
-      contributions: 1,
-      subtractions: 0,
-      profit: 0,
-      unavailablePrice: 0,
-      price: 100,
-    },
-    {
-      isin: 'B2',
-      name: 'B',
-      ongoingChargesFigure: 0.001,
-      contributions: 0,
-      subtractions: 0,
-      profit: 0,
-      unavailablePrice: 0,
-      price: 10,
-      activeFund: true,
-    },
-  ];
-  const secondPillarSourceFundsHighFee = [
-    {
-      isin: 'EE3600109435',
-      name: 'Our Fund',
-      ongoingChargesFigure: 0.004,
-      contributions: 1,
-      subtractions: 0,
-      profit: 0,
-      unavailablePrice: 0,
-      price: 0,
-    },
-    {
-      isin: 'B2',
-      name: 'B',
-      ongoingChargesFigure: 0.1,
-      contributions: 10,
-      subtractions: 0,
-      profit: 0,
-      unavailablePrice: 0,
-      price: 100000,
-      activeFund: true,
-    },
-  ];
-
   beforeEach(() => {
-    const props = {
-      secondPillarSourceFunds: secondPillarSourceFundsLowFee,
-      weightedAverageFee: 0.0049,
-    };
-    component = shallow(<BackToPartner {...props} />);
+    component = shallow(<BackToPartner />);
   });
 
   it('shows the user default success message and profile button', () => {
@@ -70,45 +19,6 @@ describe('Third pillar success step', () => {
     expect(
       component.contains(
         <FormattedMessage id="thirdPillarBackToPartner.recurringPayment.button" />,
-      ),
-    ).toBe(true);
-  });
-
-  it.skip('shows the user a high fee success message and cta button', () => {
-    // TODO: test the non-default flow
-    component.setProps({
-      secondPillarSourceFunds: secondPillarSourceFundsHighFee,
-      weightedAverageFee: 0.0051,
-    });
-
-    expect(component.contains(<FormattedMessage id="thirdPillarBackToPartner.opened" />)).toBe(
-      true,
-    );
-    expect(component.contains(<FormattedMessage id="thirdPillarSuccess.notice.header" />)).toBe(
-      true,
-    );
-    expect(component.contains(<FormattedMessage id="thirdPillarSuccess.ourFund" />)).toBe(true);
-    expect(component.contains(<FormattedMessage id="thirdPillarSuccess.currentFund" />)).toBe(true);
-
-    expect(
-      component.contains(
-        <FormattedMessage
-          id="thirdPillarSuccess.notice.description"
-          values={{
-            currentFundsFee: 0.51,
-            currentFundsFeeAmount: 510,
-            ourFundFeeAmount: 400,
-            savingsAmount: 110,
-          }}
-        />,
-      ),
-    ).toBe(true);
-
-    expect(
-      component.contains(
-        <a className="btn btn-primary mt-4 profile-link" href="/2nd-pillar-flow">
-          <FormattedMessage id="thirdPillarSuccess.button" />
-        </a>,
       ),
     ).toBe(true);
   });
