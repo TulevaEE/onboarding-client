e23e9689b6fc16f1510970b9679badfea8cd836f
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2024-04-12 13:28:50 +0700

    Work in progress: Comparison Calculator, fetch selection data

--- src/components/account/ComparisonCalculator/ComparisonCalculator.tsx
+++ src/components/account/ComparisonCalculator/ComparisonCalculator.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ComparisonCalculator.scss';
 import { FormattedMessage } from 'react-intl';
+import { useSelector } from 'react-redux';
+import moment, { Moment } from 'moment/moment';
 import { formatAmountForCurrency } from '../../common/utils';
+import { Fund } from '../../common/apiModels';
+import Select from '../ReturnComparison/select';
+import { Key } from '../ReturnComparison/api';
+import {
+  INCEPTION,
+  START_DATE,
+  THIRD_PILLAR_INCEPTION,
+} from '../ReturnComparison/ReturnComparison';
 
 interface Option {
   value: string;
@@ -25,8 +35,39 @@ interface GraphProperties {
   };
 }
 
+interface RootState {
+  exchange: { targetFunds: Fund[] };
+  thirdPillar: { funds: Fund[] };
+  login: {
+    user?: {
+      secondPillarOpenDate: string;
+      thirdPillarInitDate: string;
+    };
+  };
+}
 const ComparisonCalculator: React.FC = () => {
-  const [selectedOption, setSelectedOption] = useState<string>('option1');
+  const [selectedPillar, setSelectedPillar] = useState<Key>(Key.SECOND_PILLAR);
+
+  const secondPillarFunds = useSelector((state: RootState) => state.exchange.targetFunds || []);
+  const thirdPillarFunds = useSelector((state: RootState) => state.thirdPillar.funds || []);
+  const secondPillarOpenDate = useSelector(
+    (state: RootState) => state.login.user?.secondPillarOpenDate || '2004-03-19',
+  );
+  const thirdPillarInitDate = useSelector(
+    (state: RootState) => state.login.user?.thirdPillarInitDate || '2004-03-19',
+  );
+
+  useEffect(() => {
+    if (secondPillarFunds.length > 0) {
+      const options = secondPillarFunds.map((fund) => ({ value: fund.isin, label: fund.name }));
+      setSecondPillarFundsOptions(options);
+    }
+    if (thirdPillarFunds.length > 0) {
+      const options = thirdPillarFunds.map((fund) => ({ value: fund.isin, label: fund.name }));
+      setThirdPillarFundsOptions(options);
+    }
+  }, [secondPillarFunds, thirdPillarFunds]);
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [graphProperties, setGraphProperties] = useState<GraphProperties>({
     barCount: 2,
@@ -55,147 +96,152 @@ const ComparisonCalculator: React.FC = () => {
     },
   });
 
+  const [secondPillarFundsOptions, setSecondPillarFundsOptions] = useState<Option[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [thirdPillarFundsOptions, setThirdPillarFundsOptions] = useState<Option[]>([]);
+
   const getTimePeriodOptions = (): Option[] => {
-    return [
-      { value: 'option1', label: 'Alates II sambas kogumise algusest (19.03.2004)' },
-      { value: 'option2', label: 'Option 2' },
-      { value: 'option3', label: 'Option 3' },
-    ];
+    return getFromDateOptions();
   };
 
   const getCompareToOptions = (): Option[] => {
-    return [
-      { value: 'option1', label: 'Maailmaturu index' },
-      { value: 'option2', label: 'Option 2' },
-      { value: 'option3', label: 'Option 3' },
-    ];
+    return secondPillarFundsOptions;
   };
 
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-    // eslint-disable-next-line no-console
-    console.log('Selected key:', event.target.value);
+  const isReady = (): boolean => {
+    return getCompareToOptions().length > 0;
   };
 
   return (
     <div className="comparison-calculator">
-      <div className="card card-primary p-4">
-        <div className="header-section">
-          <div className="row justify-content-center">
-            <div className="btn-group">
-              <button type="button" className="btn btn-primary">
-                <FormattedMessage id="comparisonCalculator.yourIIpillar" />
-              </button>
-              <button type="button" className="btn btn-light">
-                <FormattedMessage id="comparisonCalculator.yourIIIpillar" />
-              </button>
-            </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-12 col-md text-left  mt-3">
-              <label htmlFor="timePeriodSelect" className="form-label">
-                <FormattedMessage id="comparisonCalculator.timePeriod" />:{' '}
-              </label>
-              <select
-                id="timePeriodSelect"
-                className="form-control"
-                value={selectedOption}
-                onChange={handleSelectChange}
-              >
-                {getTimePeriodOptions().map((option: Option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="col-12 col-md text-left  mt-3">
-              <label htmlFor="comparedToSelect" className="form-label">
-                <FormattedMessage id="comparisonCalculator.comparedTo" />:{' '}
-              </label>
-              <select
-                id="comparedToSelect"
-                className="form-control"
-                value={selectedOption}
-                onChange={handleSelectChange}
-              >
-                {getCompareToOptions().map((option: Option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className="content-section row justify-content-center">
-          <div className="col-md-7 order-2 order-md-1 d-flex flex-column">
-            <div className="result-section text-left mt-5 pb-0 d-flex flex-column justify-content-between">
-              <div className="mb-3">
-                <p className="result-text">
-                  <FormattedMessage
-                    id="comparisonCalculator.contentPerformance"
-                    values={{
-                      b: (chunks: string) => (
-                        <strong style={{ fontWeight: 'bold' }}>{chunks}</strong>
-                      ),
-                    }}
-                  />{' '}
-                  <span className="result-negative">-10 700 €</span>{' '}
-                  <FormattedMessage id="comparisonCalculator.contentPerformanceNegativeVerdict" />
-                </p>
-              </div>
-              <div className="mb-3">
-                <p className="result-text">
-                  <FormattedMessage
-                    id="comparisonCalculator.contentExplanation"
-                    values={{
-                      b: (chunks: string) => (
-                        <strong style={{ fontWeight: 'bold' }}>{chunks}</strong>
-                      ),
-                    }}
-                  />{' '}
-                </p>
-              </div>
-              <div className="">
-                <button type="button" className="btn btn-outline-primary">
-                  <FormattedMessage id="comparisonCalculator.ctaButton" />
+      {isReady() ? (
+        <div className="card card-primary p-4">
+          <div className="header-section">
+            <div className="row justify-content-center">
+              <div className="btn-group">
+                <button
+                  type="button"
+                  className={`btn ${
+                    selectedPillar === Key.SECOND_PILLAR ? 'btn-primary' : 'btn-light'
+                  }`}
+                  onClick={() => setSelectedPillar(Key.SECOND_PILLAR)}
+                >
+                  <FormattedMessage id="comparisonCalculator.yourIIpillar" />
+                </button>
+                <button
+                  type="button"
+                  className={`btn ${
+                    selectedPillar === Key.THIRD_PILLAR ? 'btn-primary' : 'btn-light'
+                  }`}
+                  onClick={() => setSelectedPillar(Key.THIRD_PILLAR)}
+                >
+                  <FormattedMessage id="comparisonCalculator.yourIIIpillar" />
                 </button>
               </div>
             </div>
-          </div>
-          {getGraphSection()}
-        </div>
-        <div className="footer-section text-center">
-          <div className="footer-disclaimer">
-            <FormattedMessage id="comparisonCalculator.footerDisclaimer" />
-          </div>
-          <div className="footer-links pt-3">
             <div className="row justify-content-center">
-              <div className="col-12 col-sm-6 col-md-auto">
-                <a
-                  href="https://tuleva.ee/mida-need-numbrid-naitavad"
-                  // className="pr-2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FormattedMessage id="comparisonCalculator.footerNumbersExplanationLink" />
-                </a>
+              <div className="col-12 col-md text-left  mt-3">
+                <label htmlFor="timePeriodSelect" className="form-label">
+                  <FormattedMessage id="comparisonCalculator.timePeriod" />:{' '}
+                </label>
+                <Select
+                  options={getTimePeriodOptions()}
+                  selected=""
+                  // selected={getTimePeriodOptions()[0].value}
+                  onChange={(key: string) => {
+                    // eslint-disable-next-line no-console
+                    console.log(key);
+                  }}
+                />
               </div>
-              <div className="col-12 col-sm-6 col-md-auto">
-                <a
-                  href="https://tuleva.ee/analuusid/millist-tootlust-on-tulevas-oodata"
-                  // className="pl-2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FormattedMessage id="comparisonCalculator.footerPerformanceExplanationLink" />
-                </a>
+              <div className="col-12 col-md text-left  mt-3">
+                <label htmlFor="comparedToSelect" className="form-label">
+                  <FormattedMessage id="comparisonCalculator.comparedTo" />:{' '}
+                </label>
+                <Select
+                  options={getCompareToOptions()}
+                  selected=""
+                  // selected={getCompareToOptions()[0].value}
+                  translate={false}
+                  onChange={(key: string) => {
+                    // eslint-disable-next-line no-console
+                    console.log(key);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="content-section row justify-content-center">
+            <div className="col-md-7 order-2 order-md-1 d-flex flex-column">
+              <div className="result-section text-left mt-5 pb-0 d-flex flex-column justify-content-between">
+                <div className="mb-3">
+                  <p className="result-text">
+                    <FormattedMessage
+                      id="comparisonCalculator.contentPerformance"
+                      values={{
+                        b: (chunks: string) => (
+                          <strong style={{ fontWeight: 'bold' }}>{chunks}</strong>
+                        ),
+                      }}
+                    />{' '}
+                    <span className="result-negative">-10 700 €</span>{' '}
+                    <FormattedMessage id="comparisonCalculator.contentPerformanceNegativeVerdict" />
+                  </p>
+                </div>
+                <div className="mb-3">
+                  <p className="result-text">
+                    <FormattedMessage
+                      id="comparisonCalculator.contentExplanation"
+                      values={{
+                        b: (chunks: string) => (
+                          <strong style={{ fontWeight: 'bold' }}>{chunks}</strong>
+                        ),
+                      }}
+                    />{' '}
+                  </p>
+                </div>
+                <div className="">
+                  <button type="button" className="btn btn-outline-primary">
+                    <FormattedMessage id="comparisonCalculator.ctaButton" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            {getGraphSection()}
+          </div>
+          <div className="footer-section text-center">
+            <div className="footer-disclaimer">
+              <FormattedMessage id="comparisonCalculator.footerDisclaimer" />
+            </div>
+            <div className="footer-links pt-3">
+              <div className="row justify-content-center">
+                <div className="col-12 col-sm-6 col-md-auto">
+                  <a
+                    href="https://tuleva.ee/mida-need-numbrid-naitavad"
+                    // className="pr-2"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FormattedMessage id="comparisonCalculator.footerNumbersExplanationLink" />
+                  </a>
+                </div>
+                <div className="col-12 col-sm-6 col-md-auto">
+                  <a
+                    href="https://tuleva.ee/analuusid/millist-tootlust-on-tulevas-oodata"
+                    // className="pl-2"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FormattedMessage id="comparisonCalculator.footerPerformanceExplanationLink" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div />
+      )}
     </div>
   );
 
@@ -256,6 +302,7 @@ const ComparisonCalculator: React.FC = () => {
     }
     return <div />;
   }
+
   function getGraphBar(properties: GraphBarProperties) {
     // eslint-disable-next-line no-nested-ternary
     const amountSign = properties.amount > 0 ? '+' : properties.amount < 0 ? '-' : '';
@@ -269,10 +316,71 @@ const ComparisonCalculator: React.FC = () => {
           <div className="bar-percentage">{properties.percentage}%</div>
         </div>
         <div className="bar-label">
-          <FormattedMessage id={properties.label} />
+          {properties.label.includes('.') ? (
+            <FormattedMessage id={properties.label} />
+          ) : (
+            properties.label
+          )}
         </div>
       </div>
     );
+  }
+
+  function getFromDateOptions(): Option[] {
+    const selectedPersonalKey = selectedPillar;
+
+    const format = (momentDate: Moment) => momentDate.format('YYYY-MM-DD');
+
+    const referenceTime =
+      selectedPersonalKey === Key.SECOND_PILLAR ? secondPillarOpenDate : thirdPillarInitDate;
+    const referenceDate = format(moment(referenceTime));
+    const beginning = new Date(START_DATE) >= new Date(referenceDate) ? START_DATE : referenceDate;
+
+    const twentyYearsAgo = format(moment().subtract(20, 'years'));
+    const fifteenYearsAgo = format(moment().subtract(15, 'years'));
+    const tenYearsAgo = format(moment().subtract(10, 'years'));
+    const fiveYearsAgo = format(moment().subtract(5, 'years'));
+    const threeYearsAgo = format(moment().subtract(3, 'years'));
+    const twoYearsAgo = format(moment().subtract(2, 'years'));
+    const oneYearAgo = format(moment().subtract(1, 'year'));
+
+    const options = [
+      { value: beginning, label: 'comparisonCalculator.period.all' },
+      ...(new Date(twentyYearsAgo) >= new Date(referenceDate)
+        ? [{ value: twentyYearsAgo, label: 'comparisonCalculator.period.twentyYears' }]
+        : []),
+      ...(new Date(fifteenYearsAgo) >= new Date(referenceDate)
+        ? [{ value: fifteenYearsAgo, label: 'comparisonCalculator.period.fifteenYears' }]
+        : []),
+      ...(new Date(tenYearsAgo) >= new Date(referenceDate)
+        ? [{ value: tenYearsAgo, label: 'comparisonCalculator.period.tenYears' }]
+        : []),
+      ...(new Date(INCEPTION) >= new Date(referenceDate)
+        ? [{ value: INCEPTION, label: 'comparisonCalculator.period.inception' }]
+        : []),
+      ...(new Date(THIRD_PILLAR_INCEPTION) >= new Date(referenceDate)
+        ? [
+            {
+              value: THIRD_PILLAR_INCEPTION,
+              label: 'comparisonCalculator.period.thirdPillarInception',
+            },
+          ]
+        : []),
+      ...(new Date(fiveYearsAgo) >= new Date(referenceDate)
+        ? [{ value: fiveYearsAgo, label: 'comparisonCalculator.period.fiveYears' }]
+        : []),
+      ...(new Date(threeYearsAgo) >= new Date(referenceDate)
+        ? [{ value: threeYearsAgo, label: 'comparisonCalculator.period.threeYears' }]
+        : []),
+      ...(new Date(twoYearsAgo) >= new Date(referenceDate)
+        ? [{ value: twoYearsAgo, label: 'comparisonCalculator.period.twoYears' }]
+        : []),
+      ...(new Date(oneYearAgo) >= new Date(referenceDate)
+        ? [{ value: oneYearAgo, label: 'comparisonCalculator.period.oneYear' }]
+        : []),
+    ];
+
+    return options.sort((option1, option2) => option1.value.localeCompare(option2.value));
   }
 };
 
