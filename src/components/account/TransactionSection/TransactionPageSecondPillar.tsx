ac8ed93060db5f1f3df80362f27ddb459a903fd7
Sander Nemvalts <snemvalts@gmail.com>
2024-05-21 14:54:36 +0300

    Rework last transactions

--- src/components/account/TransactionSection/TransactionPageSecondPillar.tsx
+++ src/components/account/TransactionSection/TransactionPageSecondPillar.tsx
@@ -2,12 +2,8 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { TransactionSection } from './TransactionSection';
 
-export const TransactionPageSecondPillar: React.FunctionComponent = () => {
-  return (
-    <>
-      <TransactionSection pillar={2}>
-        <FormattedMessage id="transactions.title.2nd" />
-      </TransactionSection>
-    </>
-  );
-};
+export const TransactionPageSecondPillar: React.FunctionComponent = () => (
+  <TransactionSection pillar={2}>
+    <FormattedMessage id="transactions.title.2nd" />
+  </TransactionSection>
+);
