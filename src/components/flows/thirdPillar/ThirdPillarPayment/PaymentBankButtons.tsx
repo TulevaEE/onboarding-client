8141d9dbc387f3e7a0377629b45b7e3629d6ddbe
Sander Nemvalts <snemvalts@gmail.com>
2024-07-04 11:37:56 +0300

    Refac 3rd pillary payment component, separate bank buttons and payment type selection

---
+++ src/components/flows/thirdPillar/ThirdPillarPayment/PaymentBankButtons.tsx
@@ -0,0 +1,20 @@
+import { BankButton } from './BankButton';
+import { BankKey } from './types';
+
+type Props = {
+  paymentBank: BankKey | 'other' | null;
+  setPaymentBank: (value: BankKey | 'other' | null) => unknown;
+};
+
+export const PaymentBankButtons = ({ paymentBank, setPaymentBank }: Props) => (
+  <>
+    <div className="mt-2 payment-banks">
+      <BankButton bankKey="swedbank" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+      <BankButton bankKey="seb" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+      <BankButton bankKey="lhv" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+      <BankButton bankKey="luminor" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+      <BankButton bankKey="coop" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+      <BankButton bankKey="other" paymentBank={paymentBank} setPaymentBank={setPaymentBank} />
+    </div>
+  </>
+);
