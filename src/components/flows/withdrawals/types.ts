f71543de88b69b21a3d848b73ac30d7195243c64
Sander Nemvalts <snemvalts@gmail.com>
2024-10-10 15:32:40 +0300

    Add types for mandatedetails

--- src/components/flows/withdrawals/types.ts
+++ src/components/flows/withdrawals/types.ts
@@ -9,3 +9,33 @@ export type WithdrawalStep = {
 export type WithdrawalStepType = WithdrawalStep['type'];
 
 export type PillarToWithdrawFrom = 'SECOND' | 'THIRD' | 'BOTH';
+
+export type WithdrawalMandateDetails =
+  | FundPensionOpeningMandateDetails
+  | PartialWithdrawalMandateDetails;
+
+export type FundPensionOpeningMandateDetails = {
+  pillar: 'SECOND' | 'THIRD';
+  // TODO remove from backend frequency: 'MONTHLY' |
+  duration: {
+    durationYears: number;
+    recommendedDuration: boolean;
+  };
+  bankAccountDetails: BankAccountDetails;
+};
+
+export type PartialWithdrawalMandateDetails = {
+  pillar: 'SECOND' | 'THIRD';
+  bankAccountDetails: BankAccountDetails;
+  fundWithdrawalAmounts: {
+    isin: string;
+    percentage: number;
+    units: number;
+  }[];
+};
+
+export type BankAccountDetails = {
+  type: 'ESTONIAN';
+  // TODO remove? bank: 'COOP' | 'SEB' | 'SWED'
+  accountIban: string;
+};
