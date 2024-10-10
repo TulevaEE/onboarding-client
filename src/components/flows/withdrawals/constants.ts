83ad1c7c7a35eb5ebb77ba20f787b068533f3e93
Sander Nemvalts <snemvalts@gmail.com>
2024-10-10 09:06:12 +0300

    Move more logic to withdrawals context

--- src/components/flows/withdrawals/constants.ts
+++ src/components/flows/withdrawals/constants.ts
@@ -1,14 +1,28 @@
+import { TranslationKey } from '../../translations';
 import { WithdrawalAmountStep } from './WithdrawalAmountStep';
 
 export type WithdrawalStep = {
   type: 'WITHDRAWAL_SIZE' | 'YOUR_INFORMATION' | 'SIGNING';
+  titleId: TranslationKey;
   component: () => JSX.Element;
 };
 
 export type WithdrawalStepType = WithdrawalStep['type'];
 
-export const withdrawalSteps: WithdrawalStep[] = [
-  { type: 'WITHDRAWAL_SIZE', component: WithdrawalAmountStep },
-  { type: 'YOUR_INFORMATION', component: WithdrawalAmountStep },
-  { type: 'SIGNING', component: WithdrawalAmountStep },
+export const WITHDRAWAL_STEPS: WithdrawalStep[] = [
+  {
+    type: 'WITHDRAWAL_SIZE',
+    titleId: 'withdrawals.content.payoutSize',
+    component: WithdrawalAmountStep,
+  },
+  {
+    type: 'YOUR_INFORMATION',
+    titleId: 'withdrawals.content.yourInformation',
+    component: WithdrawalAmountStep,
+  },
+  {
+    type: 'SIGNING',
+    titleId: 'withdrawals.content.confirmApplication',
+    component: WithdrawalAmountStep,
+  },
 ];
