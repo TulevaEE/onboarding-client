6e6bcabeef781fe761bfa33f2340abe7c334b8d0
Sander Nemvalts <snemvalts@gmail.com>
2024-11-25 12:46:39 +0100

    Add test mode indication

--- src/components/LoggedInApp/footer/Footer.spec.js
+++ src/components/LoggedInApp/footer/Footer.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { FormattedMessage } from 'react-intl';
-import Footer from './Footer';
+import { Footer } from '.';
 
 describe('Footer', () => {
   it('renders info about the company', () => {
