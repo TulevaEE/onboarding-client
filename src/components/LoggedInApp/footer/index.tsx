1ecd5fbab0129aa2c7241e197a7d58721dad837e
Taimar <tteetlok@gmail.com>
2025-02-20 23:31:16 +0700

    Better handling of text wrapping in footer; small writing style tweaks

--- src/components/LoggedInApp/footer/index.tsx
+++ src/components/LoggedInApp/footer/index.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-const Separator = () => <span className="mx-2 text-body-secondary">·</span>;
-
 export const Footer = () => {
   const { formatMessage } = useIntl();
   return (
-    <footer className="mt-5 pt-4 pb-5 border-top small text-center text-body-secondary app-footer">
-      <address>
+    <footer className="mt-5 pt-4 pb-5 border-top app-footer">
+      <address className="d-flex flex-wrap gap-4 row-gap-1 align-items-baseline justify-content-center small text-body-secondary">
         <strong>
           <FormattedMessage id="footer.name" />
         </strong>
-        <Separator />
         <FormattedMessage id="footer.address" />
-        <Separator />
         <a href={`mailto:${formatMessage({ id: 'footer.email' })}`}>
           <FormattedMessage id="footer.email" />
         </a>
-        <Separator />
         <a href={`tel:${formatMessage({ id: 'footer.phone.number' }).replace(/\s/g, '')}`}>
           <FormattedMessage id="footer.phone.number" />
         </a>
-        <Separator />
         <FormattedMessage id="footer.registration.code" />
       </address>
     </footer>
