da91f925a16703ba6d758de36c55def1634bbc1e
Taimar <tteetlok@gmail.com>
2025-03-17 14:15:26 +0700

    Add page title to ContactDetailsPage; update translations

--- src/components/contact-details/ContactDetailsPage.js
+++ src/components/contact-details/ContactDetailsPage.js
@@ -3,19 +3,24 @@ import { FormattedMessage } from 'react-intl';
 import { PropTypes as Types } from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { usePageTitle } from '../common/usePageTitle';
 import UpdateUserForm from './updateUserForm';
 import { updateUser } from '../common/user/actions';
 
 const noop = () => null;
 
-export const ContactDetailsPage = ({ saveUser }) => (
-  <div className="col-sm-8 col-md-6 col-lg-5 mx-auto">
-    <h1 className="mb-4">
-      <FormattedMessage id="update.user.details.title" />
-    </h1>
-    <UpdateUserForm onSubmit={saveUser} />
-  </div>
-);
+export const ContactDetailsPage = ({ saveUser }) => {
+  usePageTitle('pageTitle.contactDetails');
+
+  return (
+    <div className="col-sm-8 col-md-6 col-lg-5 mx-auto">
+      <h1 className="mb-4">
+        <FormattedMessage id="update.user.details.title" />
+      </h1>
+      <UpdateUserForm onSubmit={saveUser} />
+    </div>
+  );
+};
 
 ContactDetailsPage.propTypes = {
   saveUser: Types.func,
