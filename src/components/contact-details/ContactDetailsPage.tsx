183dc055f9fb3d3eca47ec7642cfb6df8fcbf214
Sander Nemvalts <snemvalts@gmail.com>
2025-03-18 13:51:00 +0200

    Refine contact details gatekeep logic

--- src/components/contact-details/ContactDetailsPage.tsx
+++ src/components/contact-details/ContactDetailsPage.tsx
@@ -1,24 +1,41 @@
 import { FormattedMessage } from 'react-intl';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect, useLocation } from 'react-router-dom';
 import { usePageTitle } from '../common/usePageTitle';
 import UpdateUserForm from './updateUserForm';
 import { updateUser } from '../common/user/actions';
+import { State } from '../../types';
+import { ContactDetailsRedirectState } from './ContactDetailsGatekeep';
 
 export const ContactDetailsPage = () => {
   const dispatch = useDispatch();
+  const location = useLocation<ContactDetailsRedirectState>();
 
-  // user
+  const updateUserSuccess = useSelector((state: State) => state.contactDetails.updateUserSuccess);
+
+  // user is redux form specific value
   const saveUser = (user: unknown) => {
     dispatch(updateUser(user));
   };
 
   usePageTitle('pageTitle.contactDetails');
 
+  if (updateUserSuccess) {
+    if ('referrer' in location.state && location.state.referrer) {
+      return <Redirect to={location.state.referrer} />;
+    }
+  }
+
   return (
     <div className="col-sm-8 col-md-6 col-lg-5 mx-auto">
       <h1 className="mb-4">
         <FormattedMessage id="update.user.details.title" />
       </h1>
+      {location.state.mandatoryUpdate && (
+        <p className="mb-5">
+          <FormattedMessage id="update.user.details.mandatoryUpdate" />
+        </p>
+      )}
       <UpdateUserForm onSubmit={saveUser} />
     </div>
   );
