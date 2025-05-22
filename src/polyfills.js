775fc8261429ef342c8e26ad2994e1f294826599
Erko Risthein <erko@risthein.ee>
2025-05-22 15:30:52 +0300

    chore: remove startsWith polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -24,9 +24,3 @@ if (isProduction) {
     },
   });
 }
-
-/* eslint-disable no-extend-native */
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = (search, pos) =>
-    this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-}
