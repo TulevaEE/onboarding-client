ae1037da679e89ecc56ac35b3cf7131b915ab09b
Taimar <tteetlok@gmail.com>
2025-06-19 16:31:58 +0300

    Add accessibility link to footer

--- src/components/LoggedInApp/footer/index.tsx
+++ src/components/LoggedInApp/footer/index.tsx
@@ -5,20 +5,31 @@ export const Footer = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <footer className="mt-5 pt-4 pb-5 border-top app-footer">
-      <address className="d-flex flex-wrap gap-4 row-gap-1 align-items-baseline justify-content-center small text-body-secondary">
-        <strong>
-          <FormattedMessage id="footer.name" />
-        </strong>
-        <FormattedMessage id="footer.address" />
-        <a href={`mailto:${formatMessage({ id: 'footer.email' })}`}>
-          <FormattedMessage id="footer.email" />
-        </a>
-        <a href={`tel:${formatMessage({ id: 'footer.phone.number' }).replace(/\s/g, '')}`}>
-          <FormattedMessage id="footer.phone.number" />
-        </a>
-        <FormattedMessage id="footer.registration.code" />
-        {/* <ContrastModeSwitch /> */}
+    <footer className="mt-5 py-5 border-top app-footer">
+      <address className="text-center small text-body-secondary">
+        <p className="d-flex flex-wrap gap-4 row-gap-1 justify-content-center">
+          <strong>
+            <FormattedMessage id="footer.name" />
+          </strong>
+          <span>
+            <FormattedMessage id="footer.address" />
+          </span>
+          <span>
+            <FormattedMessage id="footer.registration.code" />
+          </span>
+        </p>
+        <p className="d-flex flex-wrap gap-4 row-gap-1 justify-content-center">
+          <a href="https://tuleva.ee/ligipaasetavus/" target="_blank" rel="noreferrer">
+            <FormattedMessage id="footer.accessibilityStatement" />
+          </a>
+          <a href={`tel:${formatMessage({ id: 'footer.phone.number' }).replace(/\s/g, '')}`}>
+            <FormattedMessage id="footer.phone.number" />
+          </a>
+          <a href={`mailto:${formatMessage({ id: 'footer.email' })}`}>
+            <FormattedMessage id="footer.email" />
+          </a>
+          {/* <ContrastModeSwitch /> */}
+        </p>
       </address>
     </footer>
   );
