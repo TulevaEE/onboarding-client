80f6203da9af4b38f1242304d47d2db23210aa32
Sander Nemvalts <snemvalts@gmail.com>
2025-06-19 10:44:57 +0300

    Move login-page specific style to LoginPage scss module

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 
 import { logo, AuthenticationLoader, ErrorAlert } from '../common';
 import { usePageTitle } from '../common/usePageTitle';
+import styles from './LoginPage.module.scss';
 
 import LoginForm from './loginForm';
 import {
@@ -43,7 +44,7 @@ export const LoginPage = ({
   }
 
   return (
-    <div className="login-page">
+    <div className={styles.loginPage}>
       <div className="container py-5">
         <div className="row justify-content-center">
           <div className="col-12 col-md-9 col-lg-7">
