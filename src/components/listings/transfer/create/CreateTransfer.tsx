bfb56eb50edd2b10cbf18e4255b6ac3a0736b826
Sander Nemvalts <snemvalts@gmail.com>
2025-07-08 13:11:33 +0300

    Contract details abstraction, mock contract status page

--- src/components/listings/transfer/create/CreateTransfer.tsx
+++ src/components/listings/transfer/create/CreateTransfer.tsx
@@ -1,45 +1,40 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
-import styles from './CreateTransfer.module.scss';
 import { CreateTransferSteps } from './CreateTransferSteps';
-import { CreateTransferProvider, useCreateTransferContext } from './hooks';
+import { CreateTransferProvider } from './hooks';
 import { ConfirmAndSign } from './steps/ConfirmAndSign';
 import { ConfirmBuyer } from './steps/ConfirmBuyer';
 import { EnterData } from './steps/EnterData';
 import { CREATE_TRANSFER_STEPS } from './types';
 import { getTransferCreatePath } from './utils';
 
-const CreateTransferForm = () => {
-  const { currentStepType } = useCreateTransferContext();
+const CreateTransferForm = () => (
+  <div className="col-12 col-md-11 col-lg-9 mx-auto p-4">
+    <h1 className="mb-4 text-center">Liikmekapitali üleandmise avaldus</h1>
 
-  return (
-    <div className="col-12 col-md-11 col-lg-9 mx-auto p-4">
-      <h1 className="mb-4 text-center">Liikmekapitali üleandmise avaldus</h1>
-
-      <section className={styles.container}>
-        <CreateTransferSteps />
-        <Switch>
-          <Route
-            path={getTransferCreatePath(CREATE_TRANSFER_STEPS[0].subPath)}
-            component={ConfirmBuyer}
-          />
-          <Route
-            path={getTransferCreatePath(CREATE_TRANSFER_STEPS[1].subPath)}
-            component={EnterData}
-          />
-          <Route
-            path={getTransferCreatePath(CREATE_TRANSFER_STEPS[2].subPath)}
-            component={ConfirmAndSign}
-          />
-          <Redirect
-            exact
-            path="/capital/transfer/create"
-            to={getTransferCreatePath(CREATE_TRANSFER_STEPS[0].subPath)}
-          />
-        </Switch>
-      </section>
-    </div>
-  );
-};
+    <section className="bg-gray-1 border rounded br-3 p-4">
+      <CreateTransferSteps />
+      <Switch>
+        <Route
+          path={getTransferCreatePath(CREATE_TRANSFER_STEPS[0].subPath)}
+          component={ConfirmBuyer}
+        />
+        <Route
+          path={getTransferCreatePath(CREATE_TRANSFER_STEPS[1].subPath)}
+          component={EnterData}
+        />
+        <Route
+          path={getTransferCreatePath(CREATE_TRANSFER_STEPS[2].subPath)}
+          component={ConfirmAndSign}
+        />
+        <Redirect
+          exact
+          path="/capital/transfer/create"
+          to={getTransferCreatePath(CREATE_TRANSFER_STEPS[0].subPath)}
+        />
+      </Switch>
+    </section>
+  </div>
+);
 
 export const CreateTransfer = () => (
   <CreateTransferProvider>
