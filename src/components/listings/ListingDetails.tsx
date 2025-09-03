b5409b05e86c7c85b90ab1e17b1085146d767cd4
Taimar <tteetlok@gmail.com>
2025-09-03 18:11:44 +0300

    Improve layout on the reply to seller/buyer page

--- src/components/listings/ListingDetails.tsx
+++ src/components/listings/ListingDetails.tsx
@@ -94,39 +94,25 @@ export const ListingDetails = () => {
   const isDifferentLanguage = config.get('language') !== listing.language;
 
   return (
-    <div className="col-12 col-md-11 col-lg-10 mx-auto">
-      <div className="my-5">
-        <h1 className="mb-4 text-center">
-          <FormattedMessage id={`capital.listings.details.heading.messageTo.${listing.type}`} />
-        </h1>
-      </div>
+    <div className="col-12 col-md-11 col-lg-8 mx-auto d-flex flex-column gap-5">
+      <h1 className="m-0 text-center">
+        <FormattedMessage id={`capital.listings.details.heading.messageTo.${listing.type}`} />
+      </h1>
 
-      <section className={styles.listingDetailsContainer}>
+      <div className="d-flex flex-column gap-5">
         {isDifferentLanguage && (
-          <div className="alert alert-info d-flex" role="alert">
+          <div className="alert alert-primary d-flex align-items-top gap-2 m-0" role="alert">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="me-2 mt-1"
               width="16"
               height="16"
+              fill="currentColor"
               viewBox="0 0 16 16"
-              fill="none"
+              className="flex-shrink-0 mt-1"
+              aria-hidden="true"
             >
-              <g clipPath="url(#clip0_3687_5210)">
-                <path
-                  d="M8 15C6.14348 15 4.36301 14.2625 3.05025 12.9497C1.7375 11.637 1 9.85652 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8C15 9.85652 14.2625 11.637 12.9497 12.9497C11.637 14.2625 9.85652 15 8 15ZM8 16C10.1217 16 12.1566 15.1571 13.6569 13.6569C15.1571 12.1566 16 10.1217 16 8C16 5.87827 15.1571 3.84344 13.6569 2.34315C12.1566 0.842855 10.1217 0 8 0C5.87827 0 3.84344 0.842855 2.34315 2.34315C0.842855 3.84344 0 5.87827 0 8C0 10.1217 0.842855 12.1566 2.34315 13.6569C3.84344 15.1571 5.87827 16 8 16Z"
-                  fill="#002F63"
-                />
-                <path
-                  d="M8.92995 6.588L6.63995 6.875L6.55795 7.255L7.00795 7.338C7.30195 7.408 7.35995 7.514 7.29595 7.807L6.55795 11.275C6.36395 12.172 6.66295 12.594 7.36595 12.594C7.91095 12.594 8.54395 12.342 8.83095 11.996L8.91895 11.58C8.71895 11.756 8.42695 11.826 8.23295 11.826C7.95795 11.826 7.85795 11.633 7.92895 11.293L8.92995 6.588ZM8.99995 4.5C8.99995 4.76522 8.8946 5.01957 8.70706 5.20711C8.51952 5.39464 8.26517 5.5 7.99995 5.5C7.73474 5.5 7.48038 5.39464 7.29285 5.20711C7.10531 5.01957 6.99995 4.76522 6.99995 4.5C6.99995 4.23478 7.10531 3.98043 7.29285 3.79289C7.48038 3.60536 7.73474 3.5 7.99995 3.5C8.26517 3.5 8.51952 3.60536 8.70706 3.79289C8.8946 3.98043 8.99995 4.23478 8.99995 4.5Z"
-                  fill="#002F63"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_3687_5210">
-                  <rect width="16" height="16" fill="white" />
-                </clipPath>
-              </defs>
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
             </svg>
             <FormattedMessage id="capital.listings.details.differentLanguage.info" />
           </div>
@@ -146,66 +132,68 @@ export const ListingDetails = () => {
             </div>
           ) : (
             // eslint-disable-next-line react/no-danger
-            <div className="card mt-1 p-3" dangerouslySetInnerHTML={{ __html: message }} />
+            <div className="card p-3 bg-gray-1" dangerouslySetInnerHTML={{ __html: message }} />
           )}
         </div>
 
-        <div className="form-check mt-3">
-          <input
-            checked={addPhoneNumber}
-            onChange={() => setAddPhoneNumber(!addPhoneNumber)}
-            type="checkbox"
-            className="form-check-input"
-            id="add-phone-checkbox"
-          />
-          <label className="form-check-label" htmlFor="add-phone-checkbox">
-            Avaldan oma telefoninumbri: {me.phoneNumber}
-          </label>
-        </div>
-
-        <div className="form-check mt-2">
-          <input
-            checked={addPersonalCode}
-            onChange={() => setAddPersonalCode(!addPersonalCode)}
-            type="checkbox"
-            className="form-check-input"
-            id="add-personal-code-checkbox"
-          />
-          <label className="form-check-label" htmlFor="add-personal-code-checkbox">
-            Avaldan oma isikukoodi: {me.personalCode}
-          </label>
-          <div className="text-secondary">
-            Müüjalt küsitakse sinu isikukoodi liikmekapitali võõrandamise avaldusel
+        <div className="d-flex flex-column gap-2">
+          <p className="m-0">
+            <strong className="form-label">Sinu kontaktandmed</strong>{' '}
+            <span className="text-secondary">
+              (
+              <Link
+                to={{
+                  pathname: '/contact-details',
+                  state: { from: `/capital/listings/${listing.id}` },
+                }}
+              >
+                uuendan
+              </Link>
+              )
+            </span>
+          </p>
+          <div className="form-check m-0">
+            <input
+              checked={addPhoneNumber}
+              onChange={() => setAddPhoneNumber(!addPhoneNumber)}
+              type="checkbox"
+              className="form-check-input"
+              id="add-phone-checkbox"
+            />
+            <label className="form-check-label" htmlFor="add-phone-checkbox">
+              Avaldan oma telefoninumbri: {me.phoneNumber}
+            </label>
+          </div>
+          <div className="form-check m-0">
+            <input
+              checked={addPersonalCode}
+              onChange={() => setAddPersonalCode(!addPersonalCode)}
+              type="checkbox"
+              className="form-check-input"
+              id="add-personal-code-checkbox"
+            />
+            <label className="form-check-label" htmlFor="add-personal-code-checkbox">
+              Avaldan oma isikukoodi: {me.personalCode}
+            </label>
+            <div className="text-secondary">
+              Müüjalt küsitakse sinu isikukoodi liikmekapitali võõrandamise avaldusel
+            </div>
           </div>
         </div>
-
-        <div className="pt-3">
-          <div>
-            <Link
-              to={{
-                pathname: '/contact-details',
-                state: { from: `/capital/listings/${listing.id}` },
-              }}
-            >
-              Soovid uuendada oma kontaktandmeid?
-            </Link>
-          </div>
-        </div>
-
-        <div className="d-flex justify-content-between mt-4 pt-4 border-top">
-          <button type="button" className="btn btn-lg btn-light" onClick={() => history.goBack()}>
-            <FormattedMessage id="capital.listings.details.button.back" />
-          </button>
-          <button
-            type="button"
-            className="btn btn-lg btn-primary"
-            disabled={sending || isLoadingPreview}
-            onClick={handleContactButtonClicked}
-          >
-            <FormattedMessage id={`capital.listings.details.button.sendTo.${listing.type}`} />
-          </button>
-        </div>
-      </section>
+      </div>
+      <div className="d-flex flex-column-reverse flex-sm-row justify-content-between pt-4 border-top gap-3">
+        <button type="button" className="btn btn-lg btn-light" onClick={() => history.goBack()}>
+          <FormattedMessage id="capital.listings.details.button.back" />
+        </button>
+        <button
+          type="button"
+          className="btn btn-lg btn-primary"
+          disabled={sending || isLoadingPreview}
+          onClick={handleContactButtonClicked}
+        >
+          <FormattedMessage id={`capital.listings.details.button.sendTo.${listing.type}`} />
+        </button>
+      </div>
     </div>
   );
 };
