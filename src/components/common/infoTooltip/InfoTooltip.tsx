ef4623e31df9a8d47e73f46ddb58c91ab2fdd642
Taimar <tteetlok@gmail.com>
2025-09-04 14:06:30 +0300

    CopyButton: start using InfoTooltip; add copy/copied tooltips; use a single button; span→button; improve accessibility; update icons; add translations

--- src/components/common/infoTooltip/InfoTooltip.tsx
+++ src/components/common/infoTooltip/InfoTooltip.tsx
@@ -8,16 +8,22 @@ type Props = {
   name?: string;
   className?: string;
   place?: Place;
+  noTrigger?: boolean;
 };
 export const InfoTooltip = ({
   children,
   name,
   className = '',
   place = 'right',
+  noTrigger = false,
 }: PropsWithChildren<Props>) => (
-  <span className={`info-tooltip info-tooltip-modern mx-1 ${className}`}>
-    <img data-tip data-for={name} src={infoImage} alt="" className="info-tooltip__image" />
-    <span data-tip data-for={name} className="info-bubble" />
+  <span className={`info-tooltip info-tooltip-modern ${noTrigger ? '' : 'mx-1'} ${className}`}>
+    {!noTrigger && (
+      <>
+        <img data-tip data-for={name} src={infoImage} alt="" className="info-tooltip__image" />
+        <span data-tip data-for={name} className="info-bubble" />
+      </>
+    )}
     <ReactTooltip id={name} place={place} type="light" effect="solid" className="react-tooltip">
       {children}
     </ReactTooltip>
