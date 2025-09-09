192cc146b446812033778b523b43c2fee85c5ccf
Taimar <tteetlok@gmail.com>
2025-09-09 11:34:12 +0300

    AddListing: replace goBack() with push('/capital/listings/')

--- src/components/listings/AddListing.tsx
+++ src/components/listings/AddListing.tsx
@@ -210,7 +210,11 @@ export const AddListing = () => {
         </div>
 
         <div className="d-flex flex-column-reverse flex-sm-row justify-content-between pt-4 border-top gap-3">
-          <button type="button" className="btn btn-lg btn-light" onClick={() => history.goBack()}>
+          <button
+            type="button"
+            className="btn btn-lg btn-light"
+            onClick={() => history.push('/capital/listings/')}
+          >
             <FormattedMessage id="capital.listings.details.button.back" />
           </button>
           <button
