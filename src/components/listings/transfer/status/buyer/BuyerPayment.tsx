7a27832d35418b92448dc7f0a25f1098dff5d91e
Taimar <tteetlok@gmail.com>
2025-09-10 10:54:12 +0300

    More mobile layout improvements

--- src/components/listings/transfer/status/buyer/BuyerPayment.tsx
+++ src/components/listings/transfer/status/buyer/BuyerPayment.tsx
@@ -95,38 +95,38 @@ export const BuyerPayment = ({
             <FormattedMessage id="capital.transfer.details.payment.title" />
           </h2>
           <div className="d-flex flex-column gap-3">
-            <div className="row">
-              <div className="col fw-bold">
+            <div className="row align-items-baseline">
+              <div className="col-4 col-sm-5 fw-bold">
                 <FormattedMessage id="capital.transfer.details.payment.receiverName" />
               </div>
-              <div className="col d-flex justify-content-between">
-                {getFullName(contract.seller)}{' '}
+              <div className="col-8 col-sm-7 d-flex justify-content-between column-gap-2 text-break">
+                <span>{getFullName(contract.seller)}</span>
                 <CopyButton textToCopy={getFullName(contract.seller)} />
               </div>
             </div>
-            <div className="row">
-              <div className="col fw-bold">
+            <div className="row align-items-baseline">
+              <div className="col-4 col-sm-5 fw-bold">
                 <FormattedMessage id="capital.transfer.details.payment.receiverIban" />
               </div>
-              <div className="col d-flex justify-content-between">
-                {contract.iban} <CopyButton textToCopy={contract.iban} />
+              <div className="col-8 col-sm-7 d-flex justify-content-between column-gap-2 text-break">
+                <span>{contract.iban}</span> <CopyButton textToCopy={contract.iban} />
               </div>
             </div>
-            <div className="row">
-              <div className="col fw-bold">
+            <div className="row align-items-baseline">
+              <div className="col-4 col-sm-5 fw-bold">
                 <FormattedMessage id="capital.transfer.details.payment.amount" />
               </div>
-              <div className="col d-flex justify-content-between">
-                {formatAmountForCurrency(totalPrice)}{' '}
+              <div className="col-8 col-sm-7 d-flex justify-content-between column-gap-2 text-break">
+                <span>{formatAmountForCurrency(totalPrice)} </span>
                 <CopyButton textToCopy={formatAmountForCount(totalPrice).replace(/\s+/g, '')} />
               </div>
             </div>
-            <div className="row">
-              <div className="col fw-bold">
+            <div className="row align-items-baseline">
+              <div className="col-4 col-sm-5 fw-bold">
                 <FormattedMessage id="capital.transfer.details.payment.reference" />
               </div>
-              <div className="col d-flex justify-content-between">
-                Tuleva ühistu liikmekapitali ost{' '}
+              <div className="col-8 col-sm-7 d-flex justify-content-between column-gap-2 text-break">
+                <span>Tuleva ühistu liikmekapitali ost</span>
                 <CopyButton textToCopy="Tuleva ühistu liikmekapitali ost" />
               </div>
             </div>
