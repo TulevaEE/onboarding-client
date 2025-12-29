734a818b2916e9ab598a0d1e2eeaa11be71e69ea
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-12-29 13:44:45 +0100

    Revert "Add maintenance message"
    This reverts commit 907aae112577051f134894e66af2643b623ca309.

--- src/components/login/Maintenance.tsx
+++ src/components/login/Maintenance.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
-export const Maintenance: React.FC = () => {
-  const { formatMessage } = useIntl();
-
-  return (
-    <>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-      <div className="material-icons">build</div>
-      <div>
-        <FormattedMessage
-          id="login.epis_maintenance"
-          values={{
-            a: (chunks: React.ReactNode) => (
-              <a href={formatMessage({ id: 'login.epis_maintenance.url' })}>{chunks}</a>
-            ),
-          }}
-        />
-      </div>
-    </>
-  );
-};
+export const Maintenance: React.FC = () => (
+  <>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <div className="material-icons">build</div>
+    <div>
+      <FormattedMessage id="login.epis_maintenance" />
+    </div>
+  </>
+);
