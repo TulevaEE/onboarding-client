8f61268d6c3717658ceb853d403f6c1676e70171
Erko Risthein <erko@risthein.ee>
2026-01-15 12:13:05 +0200

    Fix feature flag year

--- src/components/common/featureFlags.ts
+++ src/components/common/featureFlags.ts
@@ -1,4 +1,4 @@
 export const isSavingsFundWithdrawalEnabled = (): boolean => {
-  const enableDate = new Date('2025-02-01T00:00:00');
+  const enableDate = new Date('2026-02-01T00:00:00');
   return new Date() >= enableDate;
 };
