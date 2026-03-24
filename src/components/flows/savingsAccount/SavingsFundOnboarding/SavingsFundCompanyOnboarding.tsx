76d6385be59437d713df3aabb9bf5ac447bb7602
Daniel Kütt <danielkutt@hey.com>
2026-03-24 14:17:40 +0200

    Add results from child step into parent component for an e2e experience

--- src/components/flows/savingsAccount/SavingsFundOnboarding/SavingsFundCompanyOnboarding.tsx
+++ src/components/flows/savingsAccount/SavingsFundOnboarding/SavingsFundCompanyOnboarding.tsx
@@ -3,16 +3,24 @@ import { CompanyOnboardingFormData } from './types';
 import { BusinessRegistryStep } from './BusinessRegistryStep';
 
 export const SavingsFundCompanyOnboarding = () => {
-  const { control } = useForm<CompanyOnboardingFormData>({
+  const { control, watch } = useForm<CompanyOnboardingFormData>({
     mode: 'onChange',
     defaultValues: {
       registryLookup: undefined,
     },
   });
 
+  const registryLookup = watch('registryLookup');
+
   return (
     <div>
       <BusinessRegistryStep control={control} />
+      {registryLookup && (
+        <div className="mt-4">
+          <p>{registryLookup.registryName}</p>
+          <p>{registryLookup.registryNumber}</p>
+        </div>
+      )}
     </div>
   );
 };
