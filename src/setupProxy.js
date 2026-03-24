5704019dc6cc467fafa8d454aef8d22ba2a69e20
Daniel Kütt <danielkutt@hey.com>
2026-03-24 14:57:00 +0200

    Swap implementation so that develop goes against local

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function setupProxy(app) {
-  const target = process.env.PROXY_TARGET || 'https://onboarding-service.tuleva.ee';
+  const target = process.env.PROXY_TARGET || 'http://localhost:9000';
   app.use(
     ['/v1', '/authenticate', '/oauth'],
     createProxyMiddleware({
