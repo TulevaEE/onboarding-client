f1ec8233266dab3bb56a15140a46c45b044cad96
Erko Risthein <erko@risthein.ee>
2026-09-22 15:31:30 +0300

    Show maintenance notice for EPIS outage on 2026-09-22 20:00-22:00

--- src/components/login/loginForm/LoginForm.js
+++ src/components/login/loginForm/LoginForm.js
@@ -50,8 +50,8 @@ export const LoginForm = ({
 
 const isMaintenanceWindow = () => {
   const currentTime = new Date();
-  const maintenanceStart = new Date('April 21, 2026 20:00:00');
-  const maintenanceEnd = new Date('April 21, 2026 22:00:00');
+  const maintenanceStart = new Date('September 22, 2026 20:00:00');
+  const maintenanceEnd = new Date('September 22, 2026 22:00:00');
   return currentTime >= maintenanceStart && currentTime <= maintenanceEnd;
 };
 
